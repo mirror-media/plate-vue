5dd1a9cf4e7600c14ae126332b348f682604a440
Kai-Chih Keith Chiang <chiangkaichih@gmail.com>
2017-01-11 10:37:02 +0800

    implement questionnaire module

---
+++ src/views/Questionnaire.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="questionnaire-view">
+    test questionnaire
+  </div>
+</template>
+<script>
+  export default {
+    name: 'questionnaire-view',
+    data () {
+      return {
+
+      }
+    },
+  }
+</script>
