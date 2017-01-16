766816f8906dbb573d3dfd7cf3496bb3b5a69d45
Kai-Chih Keith Chiang <chiangkaichih@gmail.com>
2017-01-16 16:22:15 +0800

    set meta description & keywords

--- src/views/Article.vue
+++ src/views/Article.vue
@@ -6,7 +6,7 @@
 </template>
 <script>
   import _ from 'lodash'
-  // import entities from 'entities'
+  import sanitizeHtml from 'sanitize-html'
   import store from '../store'
   const fetchArticles = (store) => {
     return store.dispatch('FETCH_ARTICLES', {
@@ -34,29 +34,33 @@
     mounted() {
     },
     metaInfo() {
-      const { brief , tags, title } = _.get(this.$store, [ 'state', 'articles', 'items', 0 ])
+      const { brief, id, tags, title } = _.get(this.$store, [ 'state', 'articles', 'items', 0 ])
+      const pureBrief = sanitizeHtml(_.get(brief, [ 'html' ], ''), { allowedTags: [ 'em' ] })
+      const pureTags = tags.map((t) => (t.name))
+      console.log(pureTags);
       // console.log(brief.html);
       // console.log(entities.decodeHTML(brief.html));
+      console.log(sanitizeHtml(brief.html, { allowedTags: [ 'em' ] }));
       return {
         title: title,
         meta: [
-          { name: 'keywords', content: '關鍵字' },
-          { name: 'description', content: '文章' },
+          { name: 'keywords', content: pureTags.toString() },
+          { name: 'description', content: pureBrief },
           { name: 'section-id', content: '文章' },
           { name: 'category-id', content: '文章' },
           { name: 'topic-id', content: '文章' },
           { name: 'DFPIP', content: '文章' },
           { name: 'twitter:card', content: '文章' },
           { name: 'twitter:title', content: title },
-          { name: 'twitter:description', content: '文章' },
+          { name: 'twitter:description', content: pureBrief },
           { name: 'twitter:image', content: '文章' },
           { property: 'og:site_name', content: '鏡傳媒 Mirror Media' },
           { property: 'og:locale', content: 'zh_TW' },
           { property: 'og:type', content: 'article' },
           { property: 'og:title', content: title },
-          { property: 'og:description', content: '文章desc' },
-          { property: 'og:url', content: '文章desc' },
-          { property: 'og:image', content: '文章desc' },
+          { property: 'og:description', content: pureBrief },
+          { property: 'og:url', content: '/posts/' + id },
+          { property: 'og:image', content: '' },
         ]
       }
     }
