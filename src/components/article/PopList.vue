214eec91acce80bede6698dde7def2fe4f2a2afe
Kai-Chih Keith Chiang <chiangkaichih@gmail.com>
2017-03-08 10:46:21 +0800

    adjust dfp ad layout on tag page and give pop-list section color label on article page

--- src/components/article/PopList.vue
+++ src/components/article/PopList.vue
@@ -11,7 +11,7 @@
             </div>
           </a>
         </div>
-        <div class="pop_item_title">
+        <div class="pop_item_title" :style="getSectionStyle(getValue(o, [ 'sections', 0 ], ''))">
           <a :href="reviseSlug(o.slug)" v-text="getTruncatedVal(o.title, 22)"></a>
         </div>
       </div>
@@ -19,12 +19,20 @@
   </div>
 </template>
 <script>
+  import { SECTION_MAP } from '../../constants'
   import { getHref, getTruncatedVal, getValue } from '../../utils/comm'
+  import _ from 'lodash'
   export default {
+    computed: {},
     methods: {
       getHref,
       getTruncatedVal,
       getValue,
+      getSectionStyle(sect) {
+        const sectionId = _.get(sect, [ 'id' ])
+        const style = { borderLeft: _.get( SECTION_MAP, [sectionId, 'borderLeft'], '7px solid rgba(140, 140, 140, 0.18);') }
+        return style
+      },
       reviseSlug(rawSlug) {
         const newSlug = rawSlug.replace('story', 'post')
         return newSlug
@@ -39,47 +47,52 @@
   }
 </script>
 <style lang="stylus" scoped>
-  .poplist-container {
-    font-size: 18px;
-    .pop_list {
-      margin-top: 10px;
-      display: flex;
-      align-content: flex-start;
-      flex-wrap: wrap;
-      justify-content: space-between;
-      .pop_item {
-        width: 31%;
-        vertical-align: top;
-        margin-bottom: 30px;
-        .pop_item_img {
-          width: 100%;
-          height: 150px;
-          background-repeat: no-repeat;
-          background-size: cover;
-          background-position: center center;
-        }
-        .pop_item_title {
-          background-color: #fff;
-          /*border: 1px solid #e0e0e0;*/
-          border-left: 7px solid rgba(140, 140, 140, 0.18);
-          border-top-width: 0;
-          line-height: 18px;
-          font-size: 13px;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          a {
-            width: 95%;
-            max-height: 100%;
-            margin: 10px 20px;
-            &:hover, &:link, &:visited {
-              color: #8c8c8c;
-              font-weight: normal;
-              border: none;
-            }
-          }
-        }
-      }
-    }
-  }
+  .poplist-container 
+    font-size 18px
+
+    .pop_list 
+      margin-top 10px
+      display flex
+      align-content flex-start
+      flex-wrap wrap
+      justify-content space-between
+
+      .pop_item 
+        width 31%
+        vertical-align top
+        margin-bottom 30px
+
+        .pop_item_img 
+          width 100%
+          height 150px
+          background-repeat no-repeat
+          background-size cover
+          background-position center center
+        
+        .pop_item_title 
+          background-color #fff
+          // border 1px solid #e0e0e0
+          // border-left 7px solid rgba(140, 140, 140, 0.18)
+          border-top-width 0
+          line-height 18px
+          font-size 13px
+          display flex
+          justify-content center
+          align-items center
+        
+          a 
+            width 95%
+            max-height 100%
+            margin 10px 20px
+        
+            &:hover, &:link, &:visited
+              color #8c8c8c
+              font-weight normal
+              border none
+            
+          
+        
+      
+    
+  
 </style>
