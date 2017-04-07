6eb497732d9d2935dd610d9b4fc5101da3fc23b5
Noah Tan <tempo0829@gmail.com>
2017-04-07 11:24:52 +0800

    add A/B Test Code

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -235,20 +235,6 @@ export default {
           { property: 'og:description', content: description },
           { property: 'og:url', content: SITE_URL },
           { property: 'og:image', content: '/public/notImage.png' },
-      ],
-      script: [
-        { innerHTML: `function utmx_section(){}function utmx(){}(function(){var
-          k='146067935-7',d=document,l=d.location,c=d.cookie;
-          if(l.search.indexOf('utm_expid='+k)>0)return;
-          function f(n){if(c){var i=c.indexOf(n+'=');if(i>-1){var j=c.
-          indexOf(';',i);return escape(c.substring(i+n.length+1,j<0?c.
-          length:j))}}}var x=f('__utmx'),xx=f('__utmxx'),h=l.hash;d.write(
-          '<sc'+'ript src="'+'http'+(l.protocol=='https:'?'s://ssl':
-          '://www')+'.google-analytics.com/ga_exp.js?'+'utmxkey='+k+
-          '&utmx='+(x?x:'')+'&utmxx='+(xx?xx:'')+'&utmxtime='+new Date().
-          valueOf()+(h?'&utmxhash='+escape(h.substr(1)):'')+
-          '" type="text/javascript" charset="utf-8"><\/sc'+'ript>')})();
-          <\/script><script>utmx('url','A/B');`, type: 'text/javascript' },
       ]
     }
   },
