cc3bfc32503fb04c5cad204f3db71b1af4307b14
Kai-Chih Keith Chiang <chiangkaichih@gmail.com>
2017-04-11 12:45:23 +0800

    separate superagent timeout and deadline

--- api/index.js
+++ api/index.js
@@ -1,4 +1,4 @@
-const { API_PROTOCOL, API_HOST, API_PORT, API_TIMEOUT, REDIS_AUTH, REDIS_MAX_CLIENT, REDIS_READ_HOST, REDIS_READ_PORT, REDIS_WRITE_HOST, REDIS_WRITE_PORT, REDIS_TIMEOUT, TWITTER_API } = require('./config')
+const { API_PROTOCOL, API_HOST, API_PORT, API_TIMEOUT, API_DEADLINE, REDIS_AUTH, REDIS_MAX_CLIENT, REDIS_READ_HOST, REDIS_READ_PORT, REDIS_WRITE_HOST, REDIS_WRITE_PORT, REDIS_TIMEOUT, TWITTER_API } = require('./config')
 const { LOCAL_PROTOCOL, LOCAL_PORT, LOCAL_HOST, SERVER_PROTOCOL, SERVER_HOST, QUESTIONNAIRE_HOST, QUESTIONNAIRE_PROTOCOL } = require('./config')
 const { SEARCH_PROTOCOL, SEARCH_HOST, SEARCH_ENDPOINT, SEARCH_API_KEY, SEARCH_API_APPID, SEARCH_API_TIMEOUT } = require('./config')
 const { YOUTUBE_PROTOCOL, YOUTUBE_HOST, YOUTUBE_PLAYLIST_ID, YOUTUBE_API_KEY, YOUTUBE_API_TIMEOUT } = require('./config')
@@ -166,7 +166,12 @@ router.get('*', (req, res) => {
                 // no data then http request
                 superagent
                     .get(apiHost + req.url)
-                    .timeout(API_TIMEOUT)
+                    .timeout(
+                        {
+                            response: API_TIMEOUT,  // Wait 5 seconds for the server to start sending,
+                            deadline: API_DEADLINE ? API_DEADLINE : 60000, // but allow 1 minute for the file to finish loading.
+                        }
+                    )
                     .end((err, response) => {
                         if (!err && response) {
                             console.log(`got data from api.((${Date.now() - s}ms)`, decodeURIComponent(req.url))
