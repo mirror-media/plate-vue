c7ad3fcc306e7494e7e15ca9266b4e90e17ed6f5
Noah Tan <tempo0829@gmail.com>
2017-04-21 16:32:28 +0800

    add Fixed DFP Component for Watch's Article

---
+++ src/components/DfpFixed.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="dfp-fixed ">
+    <slot name="dfpFF"></slot>
+    <img src="/public/icon/close-btn.png" @click="closeDfpFixed()">
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'dfpFixed',
+  methods: {
+    closeDfpFixed () {
+      this.$emit('closeDfpFixed')
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.dfp-fixed
+  display none
+  position fixed
+  top 165px
+  right 15px
+  > img
+    position absolute
+    top -15px
+    right -15px
+    width 30px
+    cursor pointer
+
+@media (min-width 1201px)
+  .dfp-fixed
+    display block
+</style>
