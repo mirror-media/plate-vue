e5a426efeb2dcab8a36c2948985516213006a0ac
ChienHsin-Chan <hcchien@gmail.com>
2017-07-29 10:44:33 +0800

    fixed the oralreading id in constant config map

--- src/constants/index.js
+++ src/constants/index.js
@@ -104,9 +104,9 @@ export const TOPIC_PROTEST_ID = '59151f0ff2179c0d0089b7d5'
 
 export const TOPIC_WATCH_ID = '586cd15c3c1f950d00ce2e78'
 
-export const TAG_INTERVIEW_ID = '59647e964bbe120f002a3281'
+export const TAG_INTERVIEW_ID = '59647e9e4bbe120f002a3282'
 
-export const TAG_ORALREADING_ID = '58ef372a7bce2b0e0048288c'
+export const TAG_ORALREADING_ID = '59647e964bbe120f002a3281'
 
 export const SITE_DESCRIPTION = '鏡傳媒以台灣為基地，是一跨平台綜合媒體，包含《鏡週刊》以及下設五大分眾內容的《鏡傳媒》網站，刊載時事、財經、人物、國際、文化、娛樂、美食旅遊、精品鐘錶等深入報導及影音內容。我們以「鏡」為名，務求反映事實、時代與人性。'
 
