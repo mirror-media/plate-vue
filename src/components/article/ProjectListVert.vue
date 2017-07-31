e28a6d0aabded9cb304860b275b1b43c85000981
Noah Tan <tempo0829@gmail.com>
2017-07-31 19:01:41 +0800

    add Article AB Test

---
+++ src/components/article/ProjectListVert.vue
@@ -0,0 +1,95 @@
+<template>
+  <section class="projectListVert">
+    <div class="projectListVert__title">專題報導</div>
+    <div class="projectListVert-list">
+      <div class="projectListVert-list__item" v-for="item in projects">
+        <a :href="`${siteUrl}/projects/${item.slug}`" class="projectListVert-list__item--img"><img :src="getImage(item, 'mobile')"></a>
+        <div class="projectListVert-list__item--text">
+          <a :href="`${siteUrl}/projects/${item.slug}`"><h2 v-text="getTruncatedVal(item.title, 15)"></h2></a>
+          <a :href="`${siteUrl}/projects/${item.slug}`"><p v-text="getTruncatedVal(sanitizeHtml( getValue(item, [ 'brief', 'html' ], ''), { allowedTags: [ ] }), 20)"></p></a>
+        </div>
+      </div>
+    </div>
+    <div class="projectListVert__link"><router-link to="/section/topic">看更多</router-link></div>
+  </section>
+</template>
+<script>
+  import _ from 'lodash'
+  import { SITE_URL } from '../../constants'
+  import { getImage, getTruncatedVal, getValue } from '../../util/comm'
+  import sanitizeHtml from 'sanitize-html'
+
+  export default {
+    props: [ 'initProjects' ],
+    data () {
+      return {
+        siteUrl: SITE_URL
+      }
+    },
+    computed: {
+      projects () {
+        return _.take(this.initProjects, 4)
+      }
+    },
+    methods: {
+      getImage,
+      getTruncatedVal,
+      getValue,
+      sanitizeHtml
+    }
+  }
+</script>
+<style lang="stylus" scoped>
+
+.projectListVert
+  width 100%
+  margin-top 10px
+  border 1px solid #dedede
+  &__title
+    padding .5em 1em
+    color #fff
+    font-size 19px
+    letter-spacing 1px
+    background-color #000
+  &-list
+    padding 1em 1em
+    div:first-child
+      margin-top 0
+    div:last-child
+      margin-bottom 0
+    &__item
+      display flex
+      justify-content space-between
+      margin 1.3em 0
+      &--img
+        position relative
+        width 48%
+        padding-top calc(48% * 0.6666)
+        img
+          position absolute
+          top 0
+          left 0
+          bottom 0
+          right 0
+          width 100%
+          height 100%
+          object-fit cover
+          object-position 50% 50%
+      &--text
+        width 48%
+        text-align justify
+        h2
+          margin 0
+          font-size 1rem
+          font-weight 700
+        p
+          margin-top .5em
+          margin-bottom 0
+          padding-top .5em
+          color rgba(0,0,0,.49)
+          border-top 1px solid rgba(0,0,0,.49)
+  &__link
+    padding .5em 1em
+    text-align center
+
+</style>
