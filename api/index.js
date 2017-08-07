be8d3581059452f424319ef9830abbed64ef2d15
Kai-Chih Keith Chiang <chiangkaichih@gmail.com>
2017-08-07 11:59:04 +0800

    write poplist, grouped-list, search, playlist data into redis

--- api/index.js
+++ api/index.js
@@ -6,7 +6,6 @@ const { SEARCH_PROTOCOL, SEARCH_HOST, SEARCH_ENDPOINT, SEARCH_API_KEY, SEARCH_AP
 const { YOUTUBE_PROTOCOL, YOUTUBE_HOST, YOUTUBE_PLAYLIST_ID, YOUTUBE_API_KEY, YOUTUBE_API_TIMEOUT } = require('./config')
 const express = require('express')
 const isProd = process.env.NODE_ENV === 'production'
-// const redis = require('redis')
 const RedisConnectionPool = require('redis-connection-pool')
 const router = express.Router()
 const superagent = require('superagent')
@@ -17,6 +16,13 @@ const loggingClient = Logging({
   projectId: GCP_PROJECT_ID,
   keyFilename: GCP_KEYFILE
 });
+const consoleLogOnDev = ({ msg, showSplitLine }) => {
+  if (!isProd) {
+    showSplitLine && console.log('####################')
+    console.log(msg)
+    showSplitLine && console.log('####################')
+  }
+}
 
 const apiHost = API_PROTOCOL + '://' + API_HOST + ':' + API_PORT
 
@@ -41,120 +47,174 @@ const redisPoolWrite = isProd ? RedisConnectionPool('myRedisPoolWrite', {
     } //options for createClient of node-redis, optional
   }) : redisPoolRead
 
+const redisFetching = (url, callback) => {
+  redisPoolRead.get(decodeURIComponent(url), function (err, data) {
+    redisPoolRead.ttl(decodeURIComponent(url), (_err, _data) => {
+      if (!_err && _data) {
+        if (_data === -1) {
+          redisPoolWrite.del(decodeURIComponent(req.url), (_e, _d) => {
+            if (_e) {
+              console.log('deleting key ', decodeURIComponent(url), 'from redis in fail ', _e)
+            }
+          })
+        }
+      } else {
+        console.log('fetching ttl in fail ', _err)
+      }
+    })
+    callback && callback({ err, data })
+  })
+}
+const redisWriting = (url, data, callback) => {
+  redisPoolWrite.set(decodeURIComponent(url), data, function (err) {
+    if(err) {
+      console.log('redis writing in fail. ', decodeURIComponent(url), err)
+    } else {
+      redisPoolWrite.expire(decodeURIComponent(url), REDIS_TIMEOUT, function(error, d) {
+        if(error) {
+          console.log('failed to set redis expire time. ', decodeURIComponent(url), err)
+        }
+      })
+    }
+  })
+}
+
 router.all('/', function(req, res, next) {
     next()
 });
 
 router.use('/grouped', function(req, res, next) {
-    let isErrorOccurred = false
-    // const query = req.query
-    superagent
-        .get(`${SERVER_PROTOCOL}://${SERVER_HOST}/json/grouped.json`)
-        .end((err, response) => {
-            if (!err && response) {
-                res.json(JSON.parse(response.text))
-            } else {
-                res.send('{\'error\':' + err + '}')
-                // res.status(500).end('Internal Error 500')
-                console.error(`error during fetch data from grouped : ${req.url}`)
-                console.error(err)  
-                isErrorOccurred = true
-            }
-        })
+  const url = `${SERVER_PROTOCOL}://${SERVER_HOST}/json/grouped.json`
+  redisFetching(url, ({ err, data }) => {
+    if (!err && data) {
+      res.json(JSON.parse(data))
+    } else {
+      superagent
+      .get(url)
+      .end((err, response) => {
+        if (!err && response) {
+          redisWriting(url, response.text)
+          res.json(JSON.parse(response.text))
+        } else {
+          res.send('{\'error\':' + err + '}')
+          // res.status(500).end('Internal Error 500')
+          console.error(`error during fetch data from grouped : ${url}`)
+          console.error(err)  
+        }
+      })
+    }
+  })
 });
 
 router.use('/playlist', function(req, res, next) {
-    let isErrorOccurred = false
-    let query = req.query
-    let url = `${YOUTUBE_PROTOCOL}://${YOUTUBE_HOST}?part=snippet&playlistId=${YOUTUBE_PLAYLIST_ID}&key=${YOUTUBE_API_KEY}`
-    superagent
-        .get(url)
-        .timeout(YOUTUBE_API_TIMEOUT)
-        .query(query)
-        .end(function(err, response) {
-            if (err) {
-                res.send(err)
-                // res.status(500).end('Internal Error 500')
-                console.error(`error during fetch data from playlist : ${req.url}`)
-                console.error(err)    
-                isErrorOccurred = true
-            } else {
-                res.json(response.body)
-            }
-        })
-
+  let query = req.query
+  let url = `${YOUTUBE_PROTOCOL}://${YOUTUBE_HOST}?part=snippet&playlistId=${YOUTUBE_PLAYLIST_ID}&key=${YOUTUBE_API_KEY}`
+  redisFetching(`${url}?${req.url}`, ({ err, data }) => {
+    if (!err && data) {
+      res.json(JSON.parse(data))
+    } else {
+      superagent
+      .get(url)
+      .timeout(YOUTUBE_API_TIMEOUT)
+      .query(query)
+      .end(function(err, response) {
+        if (err) {
+          res.send(err)
+          // res.status(500).end('Internal Error 500')
+          console.error(`error during fetch data from playlist : ${req.url}`)
+          console.error(err)    
+        } else {
+          redisWriting(`${url}?${req.url}`, JSON.stringify(response.body))
+          res.json(response.body)
+        }
+      })
+    }
+  })
 });
 
 router.use('/poplist', function(req, res, next) {
-    let isErrorOccurred = false
-    // const query = req.query
-    superagent
-        .get(`${SERVER_PROTOCOL}://${SERVER_HOST}/json/popularlist.json`)
-        .end((err, response) => {
-            if (!err && response) {
-                res.json(JSON.parse(response.text))
-            } else {
-                res.send('{\'error\':' + err + '}')
-                // res.status(500).end('Internal Error 500')
-                console.error(`error during fetch data from poplist : ${req.url}`)
-                console.error(err) 
-                isErrorOccurred = true  
-            }
-        })
-
+  const url = `${SERVER_PROTOCOL}://${SERVER_HOST}/json/popularlist.json`
+  redisFetching(url, ({ err, data }) => {
+    if (!err && data) {
+      res.json(JSON.parse(data))
+    } else {
+      superagent
+      .get(url)
+      .end((err, response) => {
+        if (!err && response) {
+          redisWriting(url, response.text)
+          res.json(JSON.parse(response.text))
+        } else {
+          res.send('{\'error\':' + err + '}')
+          // res.status(500).end('Internal Error 500')
+          console.error(`error during fetch data from poplist : ${url}`)
+          console.error(err) 
+        }
+      })
+    }
+  })
 });
 
 router.use('/questionnaire', function(req, res, next) {
-  let isErrorOccurred = false
   const query = req.query
+  const url = `${SERVER_PROTOCOL}://${SERVER_HOST}/questionnaire/${query.file}`
   if (!('file' in query) || query.file === '') {
-      res.send('empty file')
+    res.send('empty file')
   } else {
-      superagent
-          .get(`${SERVER_PROTOCOL}://${SERVER_HOST}/questionnaire/${query.file}`)
-          .end((err, response) => {
-              if (!err && response) {
-                  res.json(JSON.parse(response.text))
-              } else {
-                  res.send('{\'error\':' + err + '}')
-                  // res.status(500).end('Internal Error 500')
-                  console.error(`error during fetch data from questionnaire : ${req.url}`)
-                  console.error(err)   
-                  isErrorOccurred = true
-              }
-          })
+    redisFetching(url, ({ err, data }) => {
+      if (!err && data) {
+        res.json(JSON.parse(data))
+      } else {
+        superagent
+        .get(url)
+        .end((err, response) => {
+          if (!err && response) {
+            redisWriting(url, response.text)
+            res.json(JSON.parse(response.text))
+          } else {
+            res.send('{\'error\':' + err + '}')
+            // res.status(500).end('Internal Error 500')
+            console.error(`error during fetch data from questionnaire : ${req.url}`)
+            console.error(err)   
+          }
+        })
+      }
+    })
   }
 
 });
 
 router.use('/search', function(req, res, next) {
-  let isErrorOccurred = false
   let query = req.query
   let url = `${SEARCH_PROTOCOL}://${SEARCH_HOST}${SEARCH_ENDPOINT}`
-  superagent
+  redisFetching(`${url}?${req.url}`, ({ err, data }) => {
+    if (!err && data) {
+      res.json(JSON.parse(data))
+    } else {
+      superagent
       .get(url)
       .timeout(SEARCH_API_TIMEOUT)
       .set('X-Algolia-API-Key', SEARCH_API_KEY)
       .set('X-Algolia-Application-Id', SEARCH_API_APPID)
       .query(query)
       .end(function(err, response) {
-          if (err) {
-              res.send(err)
-              // res.status(500).end('Internal Error 500')
-              console.error(`error during fetch data from search : ${req.url}`)
-              console.error(err)    
-              isErrorOccurred = true
-          } else {
-              res.json(response.body)
-          }
+        if (err) {
+          res.send(err)
+          // res.status(500).end('Internal Error 500')
+          console.error(`error during fetch data from search : ${req.url}`)
+          console.error(err)    
+        } else {
+          redisWriting(`${url}?${req.url}`, JSON.stringify(response.body))
+          res.json(response.body)
+        }
       })
-
+    }
+  })
 });
 
 router.use('/twitter', function(req, res, next) {
   const query = req.query
   let client = new Twitter(TWITTER_API)
-  let isErrorOccurred = false
   if (!('screen_name' in query) || query.screen_name === '') {
       res.send('empty screen_name')
   } else {
@@ -164,7 +224,6 @@ router.use('/twitter', function(req, res, next) {
               // res.status(500).end('Internal Error 500')
               console.error(`error during fetch data from twitter : ${req.url}`)
               console.error(err) 
-              isErrorOccurred = true             
           } else {
               res.json(data)
           }
@@ -195,84 +254,45 @@ router.use('/drafts', function(req, res, next) {
 })
 
 router.get('*', (req, res) => {
-    let isErrorOccurred = false;
     res.header('Cache-Control', 'public, max-age=300');
     res.header("Access-Control-Allow-Origin", "*")
     res.header("Access-Control-Allow-Headers", "X-Requested-With")
     console.log(apiHost)
     console.log(decodeURIComponent(req.url))
-    redisPoolRead.get(decodeURIComponent(req.url), function (err, data) {
-        try {
-            if (!err && data) {
-                // have data
-                // console.log('##########', 'GOT DATA FROM REDIS', '##########')
-                // console.log(decodeURIComponent(req.url))
-                // console.log('##########', 'GOT DATA FROM REDIS', '##########')
-                redisPoolRead.ttl(decodeURIComponent(req.url), (_err, _data) => {
-                  if (!_err && _data) {
-                    if (_data === -1) {
-                      redisPoolWrite.del(decodeURIComponent(req.url), (_e, _d) => {
-                        if (_e) {
-                          console.log('deleting key ', decodeURIComponent(req.url), 'from redis in fail ', _err)
-                        }
-                      })
-                    }
-                  } else {
-                    console.log('fetching ttl in fail ', _err)
-                  }
-                })
-                
-                res.json(JSON.parse(data))
-            } else {
-                // no data then http request
-                superagent
-                    .get(apiHost + req.url)
-                    .timeout(
-                        {
-                            response: API_TIMEOUT,  // Wait 5 seconds for the server to start sending,
-                            deadline: API_DEADLINE ? API_DEADLINE : 60000, // but allow 1 minute for the file to finish loading.
-                        }
-                    )
-                    .end((err, response) => {
-                        if (!err && response) {
-                            // console.log('##########', 'GOT DATA FROM API', '##########')
-                            // console.log(decodeURIComponent(req.url))
-                            // console.log('##########', 'GOT DATA FROM API', '##########')                          
-                            const res_data = JSON.parse(response.text)
-                            const res_num = _.get(res_data, [ '_meta', 'total' ])
-                            if (res_num && res_num > 0) {
-                              res.send(res_data)
-                              redisPoolWrite.set(decodeURIComponent(req.url), response.text, function (err) {
-                                  if(err) {
-                                      console.log('redis writing in fail. ', decodeURIComponent(req.url), err)
-                                  } else {
-                                      redisPoolWrite.expire(decodeURIComponent(req.url), REDIS_TIMEOUT, function(error, d) {
-                                          if(error) {
-                                              console.log('failed to set redis expire time. ', decodeURIComponent(req.url), err)
-                                          }
-                                      })
-                                  }
-                              })
-                            } else {
-                              res.send(res_data)
-                            }
-                        } else {
-                            res.send(err)
-                            console.error(`error during fetch data : ${req.url}`)
-                            console.error(err)  
-                            isErrorOccurred = true
-                        }
-                    })
+    try {
+      redisFetching(req.url, ({ err, data }) => {
+        if (!err && data) {
+          // consoleLogOnDev({ msg: `GOT DATA FROM REDIS: \n${decodeURIComponent(req.url)}`, showSplitLine: true })
+          res.json(JSON.parse(data))
+        } else {
+          superagent
+          .get(apiHost + req.url)
+          .timeout(
+            {
+              response: API_TIMEOUT,  // Wait 5 seconds for the server to start sending,
+              deadline: API_DEADLINE ? API_DEADLINE : 60000, // but allow 1 minute for the file to finish loading.
             }
-        } catch (e) {
-            res.send(e)
-            console.error(`error during fetch data from api : ${req.url}`)
-            console.error(e)
-            isErrorOccurred = true
+          ).end((error, response) => {
+            if (!error && response) {
+              const res_data = JSON.parse(response.text)
+              const res_num = _.get(res_data, [ '_meta', 'total' ])
+              if (res_num && res_num > 0) {
+                redisWriting(req.url, response.text)
+              }
+              res.send(res_data)
+            } else {
+              res.send(error)
+              console.error(`error during fetch data : ${req.url}`)
+              console.error(error)  
+            }
+          })
         }
-    });
-
-    
+      })
+    } catch (e) {
+      res.send(e)
+      console.error(`error during fetch data from api : ${req.url}`)
+      console.error(e)
+    }
 })
 
 module.exports = router
