9d463bca83062181ab996b538abc512da382b4bd
Noah Tan <tempo0829@gmail.com>
2017-08-22 18:43:59 +0800

    add Article AB Test

--- src/views/Article.vue
+++ src/views/Article.vue
@@ -188,14 +188,14 @@
       const pureTags = _.map(tags, (t) => (_.get(t, [ 'name' ], '')))
       const sectionName = _.get(sections, [ 0, 'name' ], '')
       const topicId = _.get(topics, [ '_id' ], '')
-      // let abIndicator
-      // if (process.env.VUE_ENV === 'client') {
-      //   abIndicator = this.getMmid()
-      // }
+      let abIndicator
+      if (process.env.VUE_ENV === 'client') {
+        abIndicator = this.getMmid()
+      }
       return {
         title: `${title} - ${SITE_TITLE_SHORT}`,
         meta: `
-          <meta name="mm-opt" content="">
+          <meta name="mm-opt" content="article${abIndicator}">
           <meta name="robots" content="${robotsValue}">
           <meta name="keywords" content="${_.get(categories, [ 0, 'title' ]) + ',' + pureTags.toString()}">
           <meta name="description" content="${pureBrief}">
@@ -478,8 +478,7 @@
         const mmid = Cookie.get('mmid')
         const role = getRole({ mmid, distribution: [
           { id: 'A', weight: 50 },
-          { id: 'B', weight: 25 },
-          { id: 'C', weight: 25 } ]
+          { id: 'B', weight: 50 } ]
         })
         return role
       },
@@ -587,13 +586,13 @@
         if (_.get(articleData, [ 'sections', 'length' ]) === 0) {
           window.ga('set', 'contentGroup1', '')
           window.ga('set', 'contentGroup2', '')
-          window.ga('set', 'contentGroup3', '')
-          // window.ga('set', 'contentGroup3', `article${this.abIndicator}`)
+          // window.ga('set', 'contentGroup3', '')
+          window.ga('set', 'contentGroup3', `article${this.abIndicator}`)
         } else {
           window.ga('set', 'contentGroup1', `${_.get(articleData, [ 'sections', '0', 'name' ])}`)
           window.ga('set', 'contentGroup2', `${_.get(articleData, [ 'categories', '0', 'name' ])}`)
-          window.ga('set', 'contentGroup3', '')
-          // window.ga('set', 'contentGroup3', `article${this.abIndicator}`)
+          // window.ga('set', 'contentGroup3', '')
+          window.ga('set', 'contentGroup3', `article${this.abIndicator}`)
         }
         window.ga('send', 'pageview', { title: `${_.get(articleData, [ 'title' ], '')} - ${SITE_TITLE_SHORT}`, location: document.location.href })
       },
@@ -667,15 +666,13 @@
       })
       this.checkIfLockJS()
       this.updateSysStage()
-      // this.abIndicator = this.getMmid()
-      this.insertMatchedContentScript()
-      // if (this.abIndicator === 'A') {
-      //   this.insertMatchedContentScript()
-      // } else if (this.abIndicator === 'B') {
-      //   this.insertKianGiScript()
-      // } else {
-      //   this.insertPopInScript()
-      // }
+      this.abIndicator = this.getMmid()
+
+      if (this.abIndicator === 'A') {
+        this.insertMatchedContentScript()
+      } else {
+        this.insertKianGiScript()
+      }
 
       if (!_.isEmpty(this.articleData)) {
         this.sendGA(this.articleData)
@@ -694,14 +691,12 @@
         })
         window.FB && window.FB.XFBML.parse()
         this.checkIfLockJS()
-        this.updateMatchedContentScript()
-        // if (this.abIndicator === 'A') {
-        //   this.updateMatchedContentScript()
-        // } else if (this.abIndicator === 'B') {
-        //   this.updateKianGiScript()
-        // } else {
-        //   this.updatePopInScript()
-        // }
+
+        if (this.abIndicator === 'A') {
+          this.updateMatchedContentScript()
+        } else {
+          this.updateKianGiScript()
+        }
 
         this.updateMediafarmersScript()
         this.sendGA(this.articleData)
