766f315baef0407ec27296d3dc32920ca47feae0
Kai-Chih Keith Chiang <chiangkaichih@gmail.com>
2017-11-03 17:14:22 +0800

    remove the console.log

--- src/components/watch101/Watch101.vue
+++ src/components/watch101/Watch101.vue
@@ -91,7 +91,6 @@
         return _.get(this.$store, [ 'state', 'images', 0 ])
       },
       hotWatchList () {
-        console.log('watches', _.get(this.$store, [ 'state', 'hotWatches' ]))
         const hotList = _.get(this.$store, [ 'state', 'hotWatches' ]) || _.get(this.$store, [ 'state', 'watches' ], [])
         return hotList
       },
