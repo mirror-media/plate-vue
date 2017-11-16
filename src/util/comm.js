047a55d5a2550d030bf55ef7c500d1346546bc43
Kai-Chih Keith Chiang <chiangkaichih@gmail.com>
2017-11-16 15:19:27 +0800

    revise ad2 display logic

--- src/util/comm.js
+++ src/util/comm.js
@@ -164,6 +164,12 @@ export function unLockJS () {
   }
 }
 
+export function consoleLogOnDev ({ msg }) {
+  if (currEnv() === 'dev') {
+    console.log(msg)
+  }
+}
+
 export function currEnv () {
   if (process.env.VUE_ENV === 'client') {
     if (location.host.indexOf(SITE_DOMAIN) === 0 || location.host.indexOf(`www.${SITE_DOMAIN}`) === 0) {
