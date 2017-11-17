d3845de4c10aa4d93158078284e32930a60a8ce5
Kai-Chih Keith Chiang <chiangkaichih@gmail.com>
2017-11-17 17:59:41 +0800

    fix article fixed ad missing issue

--- src/views/ArticleM.vue
+++ src/views/ArticleM.vue
@@ -342,7 +342,7 @@
                 })
                 break
               case 'PCFF':
-                this.showDfpFixedBtn = !adDisplayStatus === 'none'
+                this.showDfpFixedBtn = !(adDisplayStatus === 'none')
                 break
             }
           },
