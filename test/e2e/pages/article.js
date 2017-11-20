c166e690d001a337e548f9fa53e4a6ec26f91b48
Kai-Chih Keith Chiang <chiangkaichih@gmail.com>
2017-11-20 18:50:30 +0800

    add article page e2e tests

--- test/e2e/pages/article.js
+++ test/e2e/pages/article.js
@@ -2,4 +2,19 @@ module.exports = function (browser) {
   this.showArticleContainer = () => {
     return browser.assert.elementPresent('.article-container')
   }
+  this.showDFPAdPoses = () => {
+    return browser
+      .assert.elementPresent('.ad-container[pos="PCHD"]')
+      .assert.elementPresent('.ad-container[pos="MBHD"]')
+      .assert.elementPresent('.ad-container[pos="PCFT"]')
+      .assert.elementPresent('.ad-container[pos="MBFT"]')
+  }
+  this.showHeroMedia = () => {
+    return browser
+      .assert.elementPresent('div.article-heromedia')
+  }
+  this.showDivAriticle = () => {
+    return browser
+      .assert.elementPresent('div.article')
+  }
 }
--- test/e2e/specs/article.spec.js
+++ test/e2e/specs/article.spec.js
@@ -3,6 +3,9 @@ module.exports = {
     browser
       .page.basic().showPageRoot('http://localhost:8080/story/20171024fin015/')
       .page.article().showArticleContainer()
+      .page.article().showDFPAdPoses()
+      .page.article().showHeroMedia()
+      .page.article().showDivAriticle()
       .end()
   }
 };
