7f41ecdcfc109d8e57beb54f2d62e3706625f0c4
Noah Tan <tempo0829@gmail.com>
2017-12-07 17:45:17 +0800

    adjust Leading slideshow img link detection

--- src/components/Leading.vue
+++ src/components/Leading.vue
@@ -5,12 +5,20 @@
         <app-slider :option="sliderOption">
           <template slot-scope="props">
             <swiper-slide :is="props.slide" v-for="(o, i) in slideshowImgs" :key="`${i}-${Date.now()}`">
-              <a :href="$_leading_getHref(o)" target="_blank">
+              <template v-if="$_leading_detectImgHref(o)">
+                <a :href="$_leading_getHref(o)" target="_blank">
+                  <img  :src="getValue(o, [ 'image', 'url' ])"
+                        :srcset="`${getValue(o, [ 'image', 'resizedTargets', 'mobile', 'url' ])} 800w,
+                                    ${getValue(o, [ 'image', 'resizedTargets', 'tablet', 'url' ])} 1200w,
+                                    ${getValue(o, [ 'image', 'resizedTargets', 'desktop', 'url' ])} 2000w`" />
+                </a>
+              </template>
+              <template v-else>
                 <img  :src="getValue(o, [ 'image', 'url' ])"
-                      :srcset="`${getValue(o, [ 'image', 'resizedTargets', 'mobile', 'url' ])} 800w,
-                                  ${getValue(o, [ 'image', 'resizedTargets', 'tablet', 'url' ])} 1200w,
-                                  ${getValue(o, [ 'image', 'resizedTargets', 'desktop', 'url' ])} 2000w`" />
-              </a>
+                        :srcset="`${getValue(o, [ 'image', 'resizedTargets', 'mobile', 'url' ])} 800w,
+                                    ${getValue(o, [ 'image', 'resizedTargets', 'tablet', 'url' ])} 1200w,
+                                    ${getValue(o, [ 'image', 'resizedTargets', 'desktop', 'url' ])} 2000w`" />
+              </template>
             </swiper-slide>
           </template>
         </app-slider>
@@ -70,6 +78,9 @@ export default {
     }
   },
   methods: {
+    $_leading_detectImgHref (item) {
+      return _.indexOf(item.keywords, '@') !== -1
+    },
     $_leading_getHref (item) {
       return _.split(item.keywords, '@-')[1]
     },
