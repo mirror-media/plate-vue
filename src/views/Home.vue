e43c2e82c881b1a81fa08e44fd23475d7957c813
Kai-Chih Keith Chiang <chiangkaichih@gmail.com>
2017-12-14 11:43:17 +0800

    send contentGroup3 as mod[abIndictor] to GA

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -418,8 +418,9 @@ export default {
 
     window.ga('set', 'contentGroup1', '')
     window.ga('set', 'contentGroup2', '')
-    window.ga('set', 'contentGroup3', '')
+    // window.ga('set', 'contentGroup3', '')
     // window.ga('set', 'contentGroup3', `home${this.abIndicator}`)
+    window.ga('set', 'contentGroup3', `mod${this.abIndicator}`)
     window.ga('send', 'pageview', { title: SITE_TITLE, location: document.location.href })
   },
   updated () {
