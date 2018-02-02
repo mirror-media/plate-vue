331ceb5a884856b898870deaeb7080d6b363f2d3
K.C.KEITH CHIANG <chiangkaichih@gmail.com>
2018-02-02 12:34:51 +0800

    fix fixed-aside layout issue (#796)

--- src/components/article/ArticleBodyExternal.vue
+++ src/components/article/ArticleBodyExternal.vue
@@ -49,8 +49,8 @@
             <slot name="latestList"></slot>
             <slot name="articleAsideFixed"></slot>
           </section>
-          <slot name="footer"></slot>
         </main>
+        <slot name="footer"></slot>
       </article>
     </div>
   </section>
