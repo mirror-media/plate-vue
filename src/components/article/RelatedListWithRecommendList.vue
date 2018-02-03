28248285258cda0de445b5661ac7a74cdfceb3e3
K.C.KEITH CHIANG <chiangkaichih@gmail.com>
2018-02-04 02:41:28 +0800

    record recomm-clicked history in localStorage (#800)

--- src/components/article/RelatedListWithRecommendList.vue
+++ src/components/article/RelatedListWithRecommendList.vue
@@ -14,8 +14,8 @@
         <template v-for="(o, i) in filteredRecommends">
           <div class="related-list__list__item" v-if="o">
             <div class="title">
-              <router-link :to="routerLinkUrl(o)" v-text="getValue(o, [ 'title' ], '')" :id="`recommend-${getValue(o, [ 'slug' ], Date.now())}`" v-if="shouldShowItem(o)"></router-link>
-              <a :href="getHrefFull(o)" v-text="getValue(o, [ 'title' ], '')" :id="`recommend-${getValue(o, [ 'slug' ], Date.now())}`" v-else></a>
+              <router-link @click.native="recommendsClickHandler(getValue(o, [ 'slug' ]), $event)" :to="routerLinkUrl(o)" v-text="getValue(o, [ 'title' ], '')" :id="`recommend-${getValue(o, [ 'slug' ], Date.now())}`" v-if="shouldShowItem(o)"></router-link>
+              <a @click="recommendsClickHandler(getValue(o, [ 'slug' ]), $event)" :href="getHrefFull(o)" v-text="getValue(o, [ 'title' ], '')" :id="`recommend-${getValue(o, [ 'slug' ], Date.now())}`" v-else></a>
             </div>
           </div>
         </template>
@@ -25,22 +25,31 @@
 </template>
 <script>
   import _ from 'lodash'
-  import { SECTION_MAP, RELATED_LIST_MAX } from '../../constants'
+  import { SECTION_MAP, RELATED_LIST_MAX, RECOMM_HITORY_MAX_IN_LOCALSTORAGE } from '../../constants'
   import { extractSlugFromreferrer, getHref, getHrefFull, getValue } from '../../util/comm'
+  import Deque from 'double-ended-queue'
+  import HashTable from 'jshashtable'
 
+  const debug = require('debug')('CLIENT:RelatedListWithRecommendList')
   export default {
     computed: {
       filteredRecommends () {
-        const origRecommendList = _.map(this.recommends, (a) => (Object.assign({}, a)))
+        const dqueue = this.getRecommClickHistory()
         const excludingArticlesLen = this.relateds.length
-        const filteredRecommendList = this.referrerSlug !== 'N/A' || this.excludingArticle !== 'N/A'
-          ? _.filter(origRecommendList, (a) => (_.get(a, [ 'slug' ]) !== this.referrerSlug) && (_.get(a, [ 'slug' ]) !== this.excludingArticle))
-          : origRecommendList
-        for (let i = 0; i < excludingArticlesLen; i += 1) {
-          _.remove(filteredRecommendList, { slug: _.get(this.relateds[ i ], [ 'slug' ]) })
-        }
-
-        return _.take(filteredRecommendList, RELATED_LIST_MAX - excludingArticlesLen)
+        const recommendListHash = this.recommendsHash
+        if (this.referrerSlug !== 'N/A') { recommendListHash.remove(this.referrerSlug) }
+        if (this.excludingArticle !== 'N/A') { recommendListHash.remove(this.excludingArticle) }
+        for (let i = 0; i < excludingArticlesLen; i += 1) { recommendListHash.remove(_.get(this.relateds[ i ], [ 'slug' ], '')) }
+        dqueue.toArray().map((slug) => { recommendListHash.remove(slug) })
+        debug(recommendListHash.values())
+        return _.take(recommendListHash.values(), RELATED_LIST_MAX - excludingArticlesLen)
+      },
+      recommendsHash () {
+        const hashtable = new HashTable()
+        _.map(this.recommends, (a) => {
+          hashtable.put(_.get(a, [ 'slug' ]), a)
+        })
+        return hashtable
       }
     },
     data () {
@@ -55,8 +64,27 @@
       containerStyle () {
         return { border: `2px solid ${_.get(SECTION_MAP, [ this.sectionId, 'bgcolor' ], '#414141')}` }
       },
+      getRecommClickHistory () {
+        const recommsClickHistory = (process.browser) && localStorage.getItem('recommsClickHistory')
+        const dqueue = new Deque(RECOMM_HITORY_MAX_IN_LOCALSTORAGE)
+        recommsClickHistory && dqueue.push(...recommsClickHistory.split(','))
+        return dqueue
+      },
       routerLinkUrl (article) {
         return !this.isApp ? getHref(article) : `/app/${getValue(article, [ 'slug' ], '')}`
+      },
+      recommendsClickHandler (slug, event) {
+        try {
+          const dqueue = this.getRecommClickHistory()
+          debug('Event click detected.', slug)
+          if (dqueue.toArray().length >= RECOMM_HITORY_MAX_IN_LOCALSTORAGE) {
+            dqueue.dequeue()
+          }
+          dqueue.enqueue(slug)
+          setTimeout(() => localStorage.setItem('recommsClickHistory', dqueue.toString()), 1000)
+        } catch (e) {
+          debug(e)
+        }
       },
       shouldShowItem (article) {
         return article.style !== 'projects' && article.style !== 'campaign' && article.style !== 'readr'
