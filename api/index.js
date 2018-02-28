1dcd3b2ee6ec724e98b170d6cda1b9115827e4ae
K.C.KEITH CHIANG <chiangkaichih@gmail.com>
2018-02-28 13:11:30 +0800

    dont cache if response is error (#807)

--- api/index.js
+++ api/index.js
@@ -293,6 +293,7 @@ router.get('*', fetchFromRedis, (req, res, next) => {
           res.send(res_data)
         } else {
           const status = _.get(response, 'status') || _.get(error, 'status') || 500
+          res.header('Cache-Control', 'no-cache')
           res.status(status).send(error)
           if (status !== 404) {
             console.error(`error during fetch data: ${req.url}`)
