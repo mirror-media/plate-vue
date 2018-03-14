c01c6c978b0998a139f3d40e45fabe15a89d8e08
Kai-Chih Keith Chiang <chiangkaichih@gmail.com>
2018-03-14 13:16:59 +0800

    implment recomm-list quantity ad test

--- src/views/Article.vue
+++ src/views/Article.vue
@@ -56,7 +56,14 @@
                 :id="`${getValue(a, [ 'pcId' ])}`" :key="`${getValue(a, [ 'pcId' ])}`"
                 class="pop_item margin-top-0" :slot="`microAd${i}`"></micro-ad>
             </pop-list>
-            <RelatedListWithRecommendList v-if="relateds.length > 0 || (recommendlist.length > 0 && !isAd)" slot="relatedlistBottom" :isAd="isAd" :sectionId="sectionId" :relateds="relateds" :recommends="recommendlist" :excludingArticle="routeUpateReferrerSlug"></RelatedListWithRecommendList>
+            <RelatedListWithRecommendList v-if="relateds.length > 0 || (recommendlist.length > 0 && !isAd)"
+              slot="relatedlistBottom" 
+              :abIndicator="abIndicator"
+              :isAd="isAd"
+              :sectionId="sectionId"
+              :relateds="relateds"
+              :recommends="recommendlist"
+              :excludingArticle="routeUpateReferrerSlug"></RelatedListWithRecommendList>
             <div class="article_fb_comment" style="margin: 1.5em 0;" slot="slot_fb_comment" v-html="fbCommentDiv"></div>
             <template slot="recommendList">
               <div><h3>推薦文章</h3></div>
