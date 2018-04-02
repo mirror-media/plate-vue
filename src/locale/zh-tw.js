7e5453bc164d006e99b402d5f6eafce6494845b9
K.C.KEITH CHIANG <chiangkaichih@gmail.com>
2018-04-02 12:55:50 +0800

    take off homepage ab test (#834)

--- src/locale/zh-tw.js
+++ src/locale/zh-tw.js
@@ -1,9 +1,10 @@
 export default {
   'mmtv': '鏡電視',
   'homepage': {
-    'editor_choice': '編輯精選'
+    'editor_choice': '編輯精選',
+    'focus': '焦點新聞',
   },
   'article': {
-    'latest': '最新文章'
-  }
+    'latest': '最新文章',
+  },
 }
