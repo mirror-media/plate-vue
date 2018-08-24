8600b98282295267ade0c518b38f2c8fe5738ab0
Kai-Wen, Hsiung <lion15945@gmail.com>
2018-08-24 15:05:53 +0800

    hide matched content when showAMPAds is false

--- api/middle/story/middle.js
+++ api/middle/story/middle.js
@@ -124,7 +124,8 @@ const sendArticleData = (req, res, next) => {
 
     storyContentAnnotation: composeAnnotation(get(find(get(res.articleData, [ 'content', 'apiData' ], []), [ 'type', 'annotation' ]), [ 'content' ], '')),
     storyRelateds: get(res.articleData, [ 'relateds' ], []),
-    showAMPAds: !get(res.articleData, 'hiddenAdvertised', false) && !isEmpty(get(DFP_UNITS, [ get(res.articleData, [ 'sections', 0, '_id' ]), 'AMP' ], {})),
+    showAMPAds: !get(res.articleData, 'hiddenAdvertised', false),
+    isAMPAdsExist: !isEmpty(get(DFP_UNITS, [ get(res.articleData, [ 'sections', 0, '_id' ]), 'AMP' ], {})),
     AMPAds: {
       DFP_ID,
       DFPUnits: get(DFP_UNITS, [ get(res.articleData, [ 'sections', 0, '_id' ]), 'AMP' ], {})
