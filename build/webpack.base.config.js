ccd8bf3d4135567e6436f349db42eefd132ffacc
Noah Tan <tempo0829@gmail.com>
2018-11-08 11:43:14 +0800

    update webpack

--- build/webpack.base.config.js
+++ build/webpack.base.config.js
@@ -1,14 +1,14 @@
 const path = require('path')
-const webpack = require('webpack')
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
-const FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin')
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin')
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 const { PUBLIC_PATH } = require('../api/config')
 const { VueLoaderPlugin } = require('vue-loader')
 
+const NODE_ENV = process.env.NODE_ENV || 'development'
 const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
+  mode: NODE_ENV,
   devtool: isProd
     ? false
     : '#cheap-module-source-map',
@@ -62,19 +62,28 @@ module.exports = {
       },
       {
         test: /\.styl(us)?$/,
-        use: isProd
-          ? ExtractTextPlugin.extract({
-              use: [
-                {
-                  loader: 'css-loader',
-                  options: { minimize: true }
-                },
-                'postcss-loader',
-                'stylus-loader'
-              ],
-              fallback: 'vue-style-loader'
-            })
-          : [ 'vue-style-loader', 'css-loader', 'postcss-loader', 'stylus-loader' ]
+        use: [
+          'vue-style-loader',
+          {
+            loader: 'css-loader',
+            options: { minimize: isProd }
+          },
+          'postcss-loader',
+          'stylus-loader'
+        ]
+        // use: isProd
+        //   ? ExtractTextPlugin.extract({
+        //       use: [
+        //         {
+        //           loader: 'css-loader',
+        //           options: { minimize: true }
+        //         },
+        //         'postcss-loader',
+        //         'stylus-loader'
+        //       ],
+        //       fallback: 'vue-style-loader'
+        //     })
+        //   : [ 'vue-style-loader', 'css-loader', 'postcss-loader', 'stylus-loader' ]
       }
     ]
   },
@@ -85,18 +94,12 @@ module.exports = {
   plugins: isProd
     ? [
         new VueLoaderPlugin(),
-        new UglifyJsPlugin({
-          uglifyOptions: {
-            warnings: false,
-          }
-        }),
-        new webpack.optimize.ModuleConcatenationPlugin(),
-        new ExtractTextPlugin({
+        new MiniCssExtractPlugin({
           filename: 'common.[chunkhash].css'
         })
       ]
     : [
         new VueLoaderPlugin(),
-        new FriendlyErrorsPlugin()
+        new FriendlyErrorsWebpackPlugin()
       ]
 }
