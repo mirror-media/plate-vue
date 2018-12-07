97d471268645311e15bd2143770244536b578303
Kai-Wen, Hsiung <kaiwenhsiung@gmail.com>
2018-12-07 01:27:12 -0600

    add Home and Article A/B test (#963)

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,9 +8,9 @@
         <vue-dfp :is="props.vueDfp" pos="LPCHD" v-if="(viewport > 999)"  :config="props.config"/>
         <vue-dfp :is="props.vueDfp" pos="LMBHD" v-else-if="(viewport < 550)" :config="props.config"/>
         <section :class="abIndicator.toLowerCase()" class="home-mainContent">
-          <editor-choice v-if="abIndicator === 'B'" :abIndicator="abIndicator" :editorChoice='editorChoice' :viewport="viewport" target="_blank" />
+          <editor-choice v-if="abIndicator === 'A'" :abIndicator="'B'" :editorChoice='editorChoice' :viewport="viewport" target="_blank" />
           <main>
-            <editor-choice v-if="abIndicator === 'A'" :abIndicator="abIndicator" :editorChoice='editorChoice' :viewport="viewport" target="_blank" />
+            <editor-choice v-if="abIndicator === 'B'" :abIndicator="'B'" :editorChoice='editorChoice' :viewport="viewport" target="_blank" />
             <vue-dfp :is="props.vueDfp" pos="LMBL1" v-if="viewport < 550" :config="props.config"/>
             <MirrorMediaTVAside v-if="viewport < 1200 && hasEventEmbedded" :mediaData="eventMod"></MirrorMediaTVAside>
             <div class="aside-title" ref="aside_title" v-show="viewport < 1200"><h2 v-text="$t('homepage.focus')"></h2></div>
@@ -548,7 +548,7 @@ export default {
 
 .home-mainContent
   width 100%
-  &.b
+  &.a
     flex-wrap wrap
   .latest-main-container
     width 90%
