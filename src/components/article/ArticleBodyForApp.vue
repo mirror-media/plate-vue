92e2a5d67bfab6f442e33a1ef2a0ea6c08af4a14
Kai-Wen, Hsiung <lion15945@gmail.com>
2018-12-17 11:05:45 +0800

    bring infobox's list-style back

--- src/components/article/ArticleBodyForApp.vue
+++ src/components/article/ArticleBodyForApp.vue
@@ -762,7 +762,11 @@ export default {
             font-size 25px
             margin-bottom 15px
           
-          .info-box-body 
+          .info-box-body
+            ol
+              list-style decimal
+            ul
+              list-style disc 
             p, li 
               color rgba(0, 0, 0, 0.64)
               font-size 16px
