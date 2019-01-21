75b549411cd1b54dadbc63bb0b129f40a9cc72a0
Noah Tan <tempo0829@gmail.com>
2019-01-21 17:23:42 +0800

    update article style and fix hero video style (#1010)

--- src/components/article/HeroImage.vue
+++ src/components/article/HeroImage.vue
@@ -1,40 +1,15 @@
 <template>
-  <div :class="[ 'hero-image', `hero-image--${abIndicator.toLowerCase()}` ]">
-    <div class="hero-image__info">
-      <div class="info-title">
-        <div
-          v-if="!isAd"
-          :style="{ borderLeftColor: get(sectionMap, [ sectionId, 'bgcolor' ]) }"
-          class="info-title__category"
-          v-text="get(articleData, 'categories.0.title', get(articleData, 'sections.0.title'))">
-        </div>
-        <h1 v-text="get(articleData, 'title')"></h1>
-        <h2 v-if="get(articleData, 'subtitle')" v-text="get(articleData, 'subtitle')"></h2>
-      </div>
-      <div v-if="heroCaption" class="info-caption" v-text="heroCaption"></div>
-    </div>
-    <div v-if="heroImage" class="hero-image__image">
-      <figure>
-        <img v-show="loading" src="/assets/mirrormedia/icon/loading.gif">
-        <img
-          v-show="!loading"
-          id="hero-image"
-          :src="images.mobile || heroImage.image.url"
-          :srcset="`${images.mobile} 800w, ${images.tablet} 1200w, ${images.desktop} 2000w`"
-          :alt="heroCaption">
-      </figure>
-      <div v-if="heroCaption" class="image-caption" v-text="heroCaption"></div>
-      <template v-if="abIndicator === 'B'">
-        <div
-          v-if="!isAd"
-          :style="{ borderLeftColor: get(sectionMap, [ sectionId, 'bgcolor' ]) }"
-          class="info-title__category"
-          v-text="get(articleData, 'categories.0.title', get(articleData, 'sections.0.title'))">
-        </div>
-        <h1 v-text="get(articleData, 'title')"></h1>
-        <h2 v-if="get(articleData, 'subtitle')" v-text="get(articleData, 'subtitle')"></h2>
-      </template>
-    </div>
+  <div class="hero-image">
+    <figure v-if="heroImage" >
+      <img v-show="loading" src="/assets/mirrormedia/icon/loading.gif">
+      <img
+        v-show="!loading"
+        id="hero-image"
+        :src="images.mobile || heroImage.image.url"
+        :srcset="`${images.mobile} 800w, ${images.tablet} 1200w, ${images.desktop} 2000w`"
+        :alt="heroCaption">
+      <figcaption v-text="heroCaption"></figcaption>
+    </figure>
   </div>
 </template>
 <script>
@@ -52,15 +27,12 @@
 
   export default {
     name: 'HeroImage',
-    props: [ 'abIndicator', 'articleData', 'heroCaption', 'heroImage', 'sectionId', 'sectionMap', 'isAd' ],
+    props: [ 'heroCaption', 'heroImage', ],
     data () {
       return {
         images: {},
         loading: false
       }
-    },
-    created () {
-      this.images = constructImages(this.heroImage)
     },
     watch: {
       '$route.fullPath' () {
@@ -73,169 +45,77 @@
       images () {
         if (this.loading) {
           const heroImage = document.querySelector('#hero-image')
-          heroImage.onload = () => {
-            this.loading = false
-          }
+          heroImage.onload = () => { this.loading = false }
         }
       }
     },
-    methods: {
-      get,
+    created () {
+      this.images = constructImages(this.heroImage)
     },
   }
 </script>
 <style lang="stylus" scoped>
   .hero-image
-    display flex
+    position relative
+    left -25px
+    width calc(100% + 50px)
     background-color #fff
-    &__info
-      display none
-    &__image
-      position relative
-      width 100%
+    figure
+      margin 0
       font-size 0
-      figure
-        margin 0
       img
         width 100%
         object-fit cover
         object-position center center
-      .image-caption
-        padding 0 50px
+      figcaption
+        padding 0 25px
         margin-top 5px
         font-size 1rem
         line-height 1.3
-    &--b
-      .hero-image__image
-        .info-title__category
-          display none
-        > h1, > h2
-          display none
+        text-align justify
 
-@media (max-width: 499px)
+@media (min-width: 500px)
   .hero-image
-    &__image
-      .image-caption
-        padding 0 25px
+    width calc(100% + 100px)
+    left -50px
+    figure
+      figcaption
+        padding 0 50px
 
 @media (min-width: 768px)
   .hero-image
-    &__image
-      .image-caption
+    position static
+    width 100%
+    figure
+      figcaption
         padding 0 calc((100% - 645px) / 2)
 
 @media (min-width: 900px)
   .hero-image
-    &__image
-      .image-caption
+    figure
+      figcaption
         padding 0 calc((100% - 695px) / 2)
 
 @media (min-width: 1200px)
   .hero-image
-    max-width 1160px
     margin 0 auto
-    &__info
-      display flex
-      flex-direction column
-      width 33.4%
-      background-color #989898
-      .info-title
-        flex 1
-        display flex
-        flex-direction column
-        justify-content center
-        padding 0 10%
-        > h1
-          margin-top 10px
-          color #fff
-          font-size 40px
-          font-weight 400
-          line-height 1.3
-          text-align justify
-        > h2
-          margin 0
-          color #fff 
-          font-size 30px 
-          font-weight 300 
-          line-height 1.3 
-          text-align justify
-        &__category
-          padding-left 10px
-          color #000
-          font-size 21px
-          border-left 7px solid #989898
-      .info-caption
-        position relative
-        padding 10px 10%
-        color #fff
-        font-weight 300
-        line-height 1.4
-        border-top 3px solid #fff
-        &::after
-          content ''
-          position absolute
-          top -9px
-          right -7.5px
-          z-index 1
-          width 15px
-          height 15px
-          background-color #fff
-          border-radius 50%
-    &__image
-      flex 1
-      width 66.6%
-      &::after
-        content ''
-        display block
-        width 100%
-        padding-top 66.66%
-      figure
-        position absolute
-        top 0 
-        left 0 
-        bottom 0 
-        right 0 
-        width 100% 
-        height 100%
-        overflow hidden
-      img
-        height 100%
-      .image-caption
-        display none
-    &--b
-      width 100%
-      .hero-image__info
-        display none
-      .hero-image__image
-        width 100%
-        &::after
-          content none
-        > h1, > h2
-          display block
-        > h1
-          margin 15px 0 25px 0
-          color #000
-          font-size 2rem
-          text-align justify
-          line-height 1.5
-        > h2
-          font-size 1.875rem
-        figure
-          position static
-          width 100%
-          height auto
-        .image-caption
-          display block
-          margin-top .5em
-          color #6e6e6e
-          line-height 1.5
-          text-align justify
-        .info-title__category
-          display block
-          margin 30px 0 0 0
-          padding-left 10px
-          color #000
-          font-size 21px
-          border-left 7px solid #989898
-        
+    figure
+      width 100% 
+      height auto
+      overflow hidden
+    figcaption
+      margin-top .5em
+      // .image-caption
+      //   display block
+      //   margin-top .5em
+      //   color #6e6e6e
+      //   line-height 1.5
+      //   text-align justify
+      // .info-title__category
+      //   display block
+      //   margin 30px 0 0 0
+      //   padding-left 10px
+      //   color #000
+      //   font-size 21px
+      //   border-left 7px solid #989898
 </style>
--- src/components/article/HeroVideo.vue
+++ src/components/article/HeroVideo.vue
@@ -1,168 +1,57 @@
 <template>
-  <!-- <div :class="[ 'heroimage-container', `heroimage-container--${abIndicator.toLowerCase()}` ]"> -->
-  <div class="heroimage-container">
-    <template v-if="viewport > 1199">
-      <!-- <template v-if="abIndicator === 'A'"> -->
-      <template>
-        <div class="hero-info">
-          <div class="hero-info-category-container">
-            <div class="hero-info-category"
-              v-if="!isAd"
-              v-text="get(articleData, [ 'categories', 0, 'title' ], get(articleData, [ 'sections', 0, 'title' ], ''))"
-              :style="{ borderLeftColor: get(sectionMap, [ sectionId, 'bgcolor' ]) }"></div>
-            <h1 v-text="get(articleData, [ 'title' ])"></h1>
-            <h2 v-if="get(articleData, [ 'subtitle' ])" v-text="get(articleData, [ 'subtitle' ])"></h2>
-          </div>
-          <div class="hero-info-heroCaption" v-text="heroCaption"></div>
-        </div>
-        <div class="hero-img">
-          <ArticleVideo v-if="video" :video="video" class="heroimg"></ArticleVideo>
-        </div>
-      </template>
-      <!-- <template v-if="abIndicator === 'B'">
-        <ArticleVideo v-if="video" :video="video" class="heroimg"></ArticleVideo>
-        <p class="hero-img-b-subtitle" v-text="heroCaption"></p>
-        <div
-          class="hero-info-category hero-info-category--b"
-          v-if="!isAd"
-          v-text="get(articleData, [ 'categories', 0, 'title' ], get(articleData, [ 'sections', 0, 'title' ], ''))"
-          :style="{ borderLeftColor: get(sectionMap, [ sectionId, 'bgcolor' ]) }"
-        >
-        </div>
-        <h1 class="hero-title-b" v-text="get(articleData, [ 'title' ])"></h1>
-        <h2 v-if="get(articleData, [ 'subtitle' ])" v-text="get(articleData, [ 'subtitle' ])"></h2>
-      </template> -->
-    </template>
-    <template v-else-if="viewport">
-      <ArticleVideo v-if="video" :video="video" class="heroimg"></ArticleVideo>
-      <div class="heroimg-caption" v-text="heroCaption" v-show="heroCaption && heroCaption.length > 0"></div>
+  <div class="hero-video">
+    <template v-if="video">
+      <ArticleVideo :video="video" class="hero-video__video"></ArticleVideo>
+      <div v-show="heroCaption && heroCaption.length > 0" class="hero-video__caption" v-text="heroCaption"></div>
     </template>
   </div>
 </template>
 <script>
-  import { get } from 'lodash'
   import ArticleVideo from 'src/components/article/Video.vue'
 
   export default {
-    name: 'HeroImage',
+    name: 'HeroVideo',
     components: {
       ArticleVideo
     },
-    methods: {
-      get
-    },
-    mounted () {},
-    props: [ 'abIndicator', 'articleData', 'heroCaption', 'sectionId', 'sectionMap', 'video', 'viewport', 'isAd' ]
+    props: [ 'heroCaption', 'video' ]
   }
 </script>
 <style lang="stylus" scoped>
-  .heroimage-container
-    margin 0 auto
-    background-color #fff
-    max-width 1160px
+  .hero-video
     position relative
-    display flex
-    justify-content space-between
-    &--b
-      margin 0
-      flex-direction column
-      justify-content flex-start
-  .hero-info
-    display flex 
-    flex-direction column
-    justify-content space-between 
-    width 33.34%
-    background-color #989898
-  .hero-info-category-container
-    display flex
-    flex-direction column
-    justify-content center
-    height 100%
-    padding 0 10%
-    > h1 
-      margin-top 10px
-      color #fff 
-      font-size 40px 
-      font-weight 400 
-      line-height 1.3 
+    left -25px
+    width calc(100% + 50px)
+    background-color #fff
+    &__caption
+      padding 0 25px
+      margin-top 5px
+      font-size 1rem
+      line-height 1.3
       text-align justify
-    > h2
-      margin 0
-      color #fff 
-      font-size 30px 
-      font-weight 300 
-      line-height 1.3 
-      text-align justify
-  .hero-info-category 
-    width 80% 
-    padding-left 10px 
-    color #000 
-    font-size 21px 
-    border-left 7px solid #989898
-    &--b
-      margin 30px 0 0 0
-  .hero-info-heroCaption 
-    position relative 
-    padding 10px 10% 
-    color #fff 
-    font-weight 300 
-    line-height 1.4 
-    border-top 3px solid #fff 
-    &::after 
-      content '' 
-      position absolute 
-      top -9px 
-      right -7.5px 
-      z-index 1 
-      width 15px 
-      height 15px 
-      background-color #fff 
-      border-radius 50% 
-  .hero-img 
-    position relative 
-    width 66.66% 
-    &[lazy=loading]
-      object-fit contain
-      height 150px
-    &::after 
-      content '' 
-      display block 
-      width 100% 
-      padding-top 66.66% 
-    > .heroimg 
-      position absolute 
-      top 0 
-      left 0 
-      bottom 0 
-      right 0 
-      width 100% 
-      height 100%
 
-  .hero-img-b
+@media (min-width: 500px)
+  .hero-video
+    width calc(100% + 100px)
+    left -50px
+    &__caption
+      padding 0 50px
+
+@media (min-width: 768px)
+  .hero-video
+    position static
     width 100%
-    height 458.7px
-    & >>> img
-      width 100%
-      height 100%
-      object-fit cover
-  .hero-img-b-subtitle
-    font-size 16px
-    line-height 1.5
-    margin 8px 0
-    color #6e6e6e
-    text-align justify
-  .hero-title-b
-    margin 15px 0 25px 0
-    text-align justify
-    line-height 1.5
-    color black
+    &__caption
+      padding 0 calc((100% - 645px) / 2)
 
-  @media (max-width: 1200px)
-    .heroimg
-      width 100%
-    .heroimage-container
-      display block
-    .heroimg-caption
-      padding 5px 25px 0
-      line-height 1.3rem
+@media (min-width: 900px)
+  .hero-video
+    &__caption
+      padding 0 calc((100% - 695px) / 2)
+
+@media (min-width: 1200px)
+  .hero-video
+    &__caption
+      margin-top .5em
+    
 </style>
