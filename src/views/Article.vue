df7f6fc1c366224d3c428e35be49e0d59ea0f5de
Noah Tan <tempo0829@gmail.com>
2019-01-24 23:36:53 +0800

    fix header logo event (#1017)

--- src/views/Article.vue
+++ src/views/Article.vue
@@ -2,7 +2,7 @@
   <vue-dfp-provider :dfpUnits="dfpUnits" :dfpid="dfpid" :section="sectionId" :options="dfpOptions" :mode="dfpMode">
     <template slot-scope="props" slot="dfpPos">
       <section style="width: 100%;">
-        <HeaderR :props="props" />
+        <HeaderR :props="props" :showDfpHeaderLogo="showDfpHeaderLogo" />
         <!-- <app-header :commonData="commonData" :eventLogo="eventLogo" :showDfpHeaderLogo="showDfpHeaderLogo" :viewport="viewport" v-if="(articleStyle !== 'photography')" :props="props"></app-header> -->
       </section>
       <div class="article-container" v-if="(articleStyle !== 'photography')" >
