bc6b62963eef94041d4a40a7cf5092e26f4dce19
Noah Tan <xueyongtan@gmail.com>
2019-02-20 19:29:52 +0800

    add home ab test (#1053)

--- src/store/api.js
+++ src/store/api.js
@@ -204,6 +204,11 @@ function loadLatestArticle (params = {}) {
   return _doFetch(url)
 }
 
+function loadLatestNewsFromJson () {
+  const url = `${_host}/api/latestNews`
+  return _doFetch(url)
+}
+
 function loadNodes (params = {}) {
   const query = _buildQuery(params)
   let url = `${_host}/api/nodes`
@@ -352,6 +357,10 @@ export function fetchImagesById (params = {}) {
 
 export function fetchLatestArticle (params = {}) {
   return loadLatestArticle(params)
+}
+
+export function fetchLatestNewsFromJson () {
+  return loadLatestNewsFromJson()
 }
 
 export function fetchNodes (params = {}) {
