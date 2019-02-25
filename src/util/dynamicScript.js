957a613b09e1cba4bc6dd7944f78222ea2bcf3ee
K.C.KEITH CHIANG <chiangkaichih@gmail.com>
2019-02-25 16:00:35 +0800

    load gtm code after DOMContentLoaded (#1073)

---
+++ src/util/dynamicScript.js
@@ -0,0 +1,14 @@
+export const gtm_mirrormedia = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-NCH86SP');`
+
+export const gtm_likr = `window.AviviD = window.AviviD || {settings:{},status:{}};
+AviviD.web_id = 'mirrormedia';
+AviviD.category_id = window.avivid_category_id || '20180905000003';
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl+'&timestamp='+new Date().getTime();f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TV7N2SW');`
--- src/util/mixinTitleMeta.js
+++ src/util/mixinTitleMeta.js
@@ -1,6 +1,9 @@
 import _ from 'lodash'
+import { gtm_mirrormedia, gtm_likr } from './dynamicScript'
 
 const debug = require('debug')('CLIENT:mixinTitleMeta')
+let isGTMLoaded = false
+let isDomContentLoadedHandlerSetup = false
 
 function getMetaSet (vm) {
   const { metaSet } = vm.$options
@@ -40,63 +43,70 @@ const serverTitleMetaMixin = {
   }
 }
 
+const updateMeta = (metaInfo, vm) => {
+  const { title, meta, url, adTrace } = metaInfo
+  const adTraceScripts = [ ...document.querySelectorAll('*[data-name="ad-trace"]') ]
+  adTraceScripts.map(node => node.remove())
+  
+  if (title) {
+    document.querySelector('title').innerHTML = title
+  }
+  if (url) {
+    const alternate = document.head.querySelector(`link[rel='alternate']`)
+    alternate && (alternate.href = url)
+  }
+  if (adTrace) {
+    const parser = new DOMParser()
+    const doc = parser.parseFromString(adTrace, "text/html")
+    const scripts = [ ...doc.querySelectorAll('*[data-name="ad-trace"]') ]
+    debug('adTrace scripts', scripts)
+    scripts.map(node => document.head.appendChild(node))
+  }
+  if (meta) {
+    const dynamicMeta = document.querySelectorAll('head meta:not([fixed="true"])')
+    const newMeta = _.split(meta, '>')
+    _.forEach(dynamicMeta, (node) => {
+      document.head.removeChild(node)
+    })
+    _.forEach(newMeta, (m) => {
+      const node = document.createElement('div')
+      node.innerHTML = `${m}>`
+      const updateMeta = node.querySelector('meta')
+      if (updateMeta) {
+        document.head.appendChild(updateMeta)
+      }
+    })
+  }
+}
+
 const clientTitleMetaMixin = {
   mounted () {
     const metaSet = getMetaSet(this)
-    if (!metaSet) { return }
-    // const meta = metaSet.meta
-    const title = metaSet.title
-    if (title) {
-      document.querySelector('title').innerHTML = title
-    }
+    metaSet && updateMeta(metaSet, this)
   },
   updated () {
     const metaSet = getMetaSet(this)
-    if (!metaSet) { return }
-    const meta = metaSet.meta
-    const title = metaSet.title
-    const url = metaSet.url
-    // const link = metaSet.link
-    const adTrace = metaSet.adTrace
-    const adTraceScripts = [ ...document.querySelectorAll('*[data-name="ad-trace"]') ]
-    debug('adTrace', adTrace)
-    adTraceScripts.map(node => node.remove())
-    
-    if (title) {
-      document.querySelector('title').innerHTML = title
-    }
-    // if (link) {
-    //   const amphtml = document.head.querySelector(`link[rel='amphtml']`)
-    //   amphtml && (amphtml.href = link)
-    // }
-    if (url) {
-      const alternate = document.head.querySelector(`link[rel='alternate']`)
-      alternate && (alternate.href = url)
-    }
-    if (adTrace) {
-      const parser = new DOMParser()
-      const doc = parser.parseFromString(adTrace, "text/html")
-      const scripts = [ ...doc.querySelectorAll('*[data-name="ad-trace"]') ]
-      debug('adTrace scripts', scripts)
-      scripts.map(node => document.head.appendChild(node))
-    }
-    if (meta) {
-      const dynamicMeta = document.querySelectorAll('head meta:not([fixed="true"])')
-      const newMeta = _.split(meta, '>')
-      _.forEach(dynamicMeta, (node) => {
-        document.head.removeChild(node)
-      })
-      _.forEach(newMeta, (m) => {
-        const node = document.createElement('div')
-        node.innerHTML = `${m}>`
-        const updateMeta = node.querySelector('meta')
-        if (updateMeta) {
-          document.head.appendChild(updateMeta)
-        }
-      })
-    }
+    metaSet && updateMeta(metaSet, this)
   }
 }
+
+process.env.VUE_ENV === 'client' && !isDomContentLoadedHandlerSetup && document.addEventListener('DOMContentLoaded', () => {
+  console.log('DOMContentLoaded')
+  if (!isGTMLoaded) {
+    console.log('isGTMLoaded', isGTMLoaded)
+    const insertCodes = async codes => {
+      const script = document.createElement('script')
+      script.innerHTML = codes
+      document.head.appendChild(script)
+    }
+    isGTMLoaded = Promise.all([
+      insertCodes(gtm_mirrormedia),
+      insertCodes(gtm_likr),
+    ]).then(() => true).catch(() => false)
+    console.log('isGTMLoaded', isGTMLoaded)
+  }
+  isDomContentLoadedHandlerSetup = true
+})
 
 export default process.env.VUE_ENV === 'server'
   ? serverTitleMetaMixin
