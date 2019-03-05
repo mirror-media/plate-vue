1e9e5caba93aca89e41986af35bc2166ae926b3d
Noah Tan <tempo0829@gmail.com>
2019-03-05 14:56:50 +0800

    update utility test and improve getBrief utility function

--- tests/unit/util/comm.test.js
+++ tests/unit/util/comm.test.js
@@ -1,5 +1,8 @@
 import {
   getArticleReadTime,
+  getAuthor,
+  getAuthorHref,
+  getBrief,
 } from '../../../src/util/comm.js'
 
 describe('getArticleReadTime', () => {
@@ -32,5 +35,83 @@ describe('getArticleReadTime', () => {
     測試用字串測試用字串測試用字串測試用字串測試用字串測試用字串測試用字串測試用字串測試用字串測試用字串
     測試用字串測試用字串測試用字串測試用字串` ] } ]
     expect(getArticleReadTime(paragraphs)).toBe(2)
+  })
+})
+
+describe('getAuthor', () => {
+  const article = {
+    writers: [
+      {
+        id: "test123",
+        name: "測試用",
+      },
+      {
+        id: "test321",
+        name: "測試測試",
+      }
+    ],
+    photographers: [
+      {
+        name: "我是攝影",
+      },
+    ],
+    designers: [
+      {
+        name: "我是設計",
+      },
+    ],
+    engineers: [
+      {
+        name: "我是工程",
+      },
+    ],
+    extendByline: '我是外部'
+  }
+  const expected = /(文｜)|測試用|測試測試|(攝影｜)|我是攝影|(設計｜)|我是設計|(工程｜)|我是工程|我是外部/
+  test('return full info', () => {
+    expect(getAuthor(article)).toMatch(expected)
+  })
+  test('return writers', () => {
+    expect(getAuthor(article, 'writers')).toMatch(/(文｜)|測試用/)
+    expect(getAuthor(article, 'writers')).not.toMatch(/(攝影｜)|(設計｜)|(工程｜)|我是外部/)
+  })
+  test('return specific delimiter', () => {
+    expect(getAuthor(article, '', '!')).toMatch(/(文!)|(攝影!)|(設計!)|(工程!)/)
+  })
+})
+
+describe('getAuthorHref', () => {
+  const author = { id: "test123", name: "測試用", }
+  test('return author page href', () => {
+    expect(getAuthorHref(author)).toEqual('/author/test123')
+  })
+})
+
+describe('getBrief', () => {
+  const article = {
+    brief: {
+      html: '<p>台灣獨立遊戲《還願》因被眼尖玩家發現藏有暗諷中國國家主席習近平的場景素材，導致中國官方全面封殺。有中國媒體工作者評論，《還願》上市前盛大的ARG（Alternate reality game，另類實境遊戲）活動，也是引爆這次事件的遠因之一。 </p>',
+    }
+  }
+  const article_only_brief = {
+    brief: '<p>台灣獨立遊戲《還願》因被眼尖玩家發現藏有暗諷中國國家主席習近平的場景素材，導致中國官方全面封殺。有中國媒體工作者評論，《還願》上市前盛大的ARG（Alternate reality game，另類實境遊戲）活動，也是引爆這次事件的遠因之一。 </p>'
+  }
+
+  const article_only_ogDescription = {
+    ogDescription: '台灣獨立遊戲《還願》因被眼尖玩家發現藏有暗諷中國國家主席習近平的場景素材，導致中國官方全面封殺。有中國媒體工作者評論，《還願》上市前盛大的ARG（Alternate reality game，另類實境遊戲）活動，也是引爆這次事件的遠因之一。 '
+  }
+  test('use article brief html to return truncated brief', () => {
+    expect(getBrief(article)).toBeString()
+    expect(getBrief(article).length).toEqual(31)
+  })
+
+  test('use article brief to return truncated brief', () => {
+    expect(getBrief(article_only_brief)).toBeString()
+    expect(getBrief(article_only_brief).length).toEqual(31)
+  })
+
+  test('use article ogDescription to return truncated brief', () => {
+    expect(getBrief(article_only_ogDescription)).toBeString()
+    expect(getBrief(article_only_ogDescription).length).toEqual(31)
   })
 })
