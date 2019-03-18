bb49c651ee9e70b588807fb6ee5dbca9a04796c4
Noah Tan <xueyongtan@gmail.com>
2019-03-18 12:50:44 +0800

    adjust common utility getHost for test (#1113)

--- src/util/comm.js
+++ src/util/comm.js
@@ -157,7 +157,7 @@ export function getValue (o = {}, p = [], d = '') { // considered deprecated
 
 export function getHost () {
   const browser = typeof window !== 'undefined'
-  if (browser) {
+  if (browser && process.env.VUE_ENV !== 'server') {
     return `//${location.host}`
   } else {
     const host = process.env.HOST || 'localhost'
