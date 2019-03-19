f056b007b44d52b7b8bed5bcee7c59b520c4953d
Noah Tan <xueyongtan@gmail.com>
2019-03-19 15:57:37 +0800

    restructure store state (#1119)

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import config from '../../api/config'
+import { DFPID, FB_APP_ID, FB_PAGES_ID } from '../../api/config'
 
 import actions from './actions'
 import mutations from './mutations'
@@ -8,60 +8,60 @@ import getters from './getters'
 
 Vue.use(Vuex)
 
-const { DFPID, FB_APP_ID, FB_PAGES_ID } = config
+const state = {
+  activities: {},
+  articles: {},
+  articlesByUUID: {
+    CATEGORY: {},
+    SECTION: {},
+    TAG: {},
+    TOPIC: {}
+  },
+  articlesPopList: {},
+  articlesRecommendList: {},
+  articlesGroupedList: {},
+  audios: {},
+  authors: [],
+  commonData: {},
+  contact: {},
+  dfpId: DFPID,
+  editorChoice: {},
+  eventEmbedded: {},
+  eventLogo: {},
+  eventMod: {},
+  external: {},
+  externals: {},
+  fbAppId: FB_APP_ID,
+  fbPagesId: FB_PAGES_ID,
+  highlightNodes: {},
+  images: {},
+  imagesById: [],
+  isTimeToShowAdCover: false,
+  latestArticle: {},
+  latestArticles: {},
+  latestNewsFromJson: {},
+  nodes: {},
+  ogimage: {},
+  playlist: {
+    info: {}
+  },
+  searchResult: {},
+  tag: {},
+  tags: [],
+  timeline: {},
+  topic: {},
+  topics: {},
+  uuid: '',
+  videos: {},
+  viewport: {
+    height: 0,
+    width: 0
+  },
+}
 
 export function createStore () {
   return new Vuex.Store({
-    state: {
-      activities: {},
-      articles: {},
-      articlesByUUID: {
-        CATEGORY: {},
-        SECTION: {},
-        TAG: {},
-        TOPIC: {}
-      },
-      articlesPopList: {},
-      articlesRecommendList: {},
-      articlesGroupedList: {},
-      audios: {},
-      authors: [],
-      commonData: {},
-      contact: {},
-      dfpId: DFPID,
-      editorChoice: {},
-      eventEmbedded: {},
-      eventLogo: {},
-      eventMod: {},
-      external: {},
-      externals: {},
-      fbAppId: FB_APP_ID,
-      fbPagesId: FB_PAGES_ID,
-      highlightNodes: {},
-      images: {},
-      imagesById: [],
-      isTimeToShowAdCover: false,
-      latestArticle: {},
-      latestArticles: {},
-      latestNewsFromJson: {},
-      nodes: {},
-      ogimage: {},
-      playlist: {
-        info: {}
-      },
-      searchResult: {},
-      tag: {},
-      tags: [],
-      timeline: {},
-      topic: {},
-      topics: {},
-      uuid: '',
-      videos: {},
-      viewport: {
-        height: 0,
-        width: 0
-      },
-    },
+    state,
     actions,
     mutations,
     getters
