2deefe3ad138ad32a7ec9b641d7d46949600fde6
Noah Tan <tempo0829@gmail.com>
2019-04-25 16:25:53 +0800

    fix dfp mode (#1193)

--- src/views/ArticleM.vue
+++ src/views/ArticleM.vue
@@ -1,5 +1,5 @@
 <template>
-  <vue-dfp-provider :dfpUnits="dfpUnits" :dfpid="dfpid" :section="sectionId" :options="dfpOptions" :mode="`prod`">
+  <vue-dfp-provider :dfpUnits="dfpUnits" :dfpid="dfpid" :section="sectionId" :options="dfpOptions" :mode="dfpMode">
     <template slot-scope="props" slot="dfpPos">
       <div class="article-container" v-if="(articleStyle !== 'photography')" >
         <!-- <vue-dfp :is="props.vueDfp" pos="PCHD" extClass="full mobile-hide" :config="props.config"/> -->
