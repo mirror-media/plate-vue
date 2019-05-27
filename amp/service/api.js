6c3ab69a4e53cbf7af0aaedae63feb7e45fbf4ee
Kai-Wen, Hsiung <lion15945@gmail.com>
2019-05-27 12:13:12 +0800

    fix amp 500 error and add logs

--- amp/service/api.js
+++ amp/service/api.js
@@ -1,7 +1,19 @@
 const express = require('express')
 const router = express.Router()
-const { validateSlugIsEmpty, fetchFromRedis, fetchStory, getArticleData, sendArticleData } = require('./middle')
+const {
+  validateSlugIsEmpty,
+  fetchFromRedis,
+  fetchStory,
+  getArticleData,
+  sendArticleData
+} = require('./middle')
 
-router.get('/:slug?', validateSlugIsEmpty, [ fetchFromRedis, fetchStory ], getArticleData, sendArticleData)
+router.get(
+  '/:slug?',
+  validateSlugIsEmpty,
+  [ fetchFromRedis, fetchStory ],
+  getArticleData,
+  sendArticleData
+)
 
 module.exports = router
