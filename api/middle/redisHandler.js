ce1a09ee1b5185d1ad41e0c84f2f5616660c8e27
Noah Tan <tempo0829@gmail.com>
2019-07-11 10:59:39 +0800

    replace redis connection pool by node_redis (#1252)

--- api/middle/redisHandler.js
+++ api/middle/redisHandler.js
@@ -1,13 +1,11 @@
 const debug = require('debug')('PLATEVUE:redis')
 const isProd = process.env.NODE_ENV === 'production'
-// const isTest = process.env.NODE_ENV === 'test'
-const RedisConnectionPool = require('redis-connection-pool')
 
 const redis = require('./redis')
+const { promisify } = require('util')
 
 const { 
   REDIS_AUTH,
-  REDIS_MAX_CLIENT,
   REDIS_READ_HOST,
   REDIS_READ_PORT,
   REDIS_WRITE_HOST,
@@ -15,110 +13,70 @@ const {
   REDIS_RECOMMEND_NEWS_HOST,
   REDIS_RECOMMEND_NEWS_PORT,
   REDIS_CONNECTION_TIMEOUT,
-  REDIS_TIMEOUT } = require('../config')
+  REDIS_TIMEOUT
+} = require('../config')
 
-const REDIS_OPTIONS = {
-  auth_pass: REDIS_AUTH,
-  retry_strategy: function (options) {
-    if (options.error && options.error.code === 'ECONNREFUSED') {
-      return new Error('The server refused the connection')
-    }
-    if (options.error && options.error.code === 'ETIMEDOUT') {
-      return new Error('Timeout occured while connecting to redis.')      
-    }
-    if (options.total_retry_time > 200 * 5) {
-      return new Error('Retry time exhausted')
-    }
-    if (options.attempt > 0 || options.times_connected > 0) {
-      // this means "dont do retry"
-      return undefined
-    }
-    // reconnect after
-    // wouldnt go this return way never
-    return 100
-  }  
+const retryStrategy = (options) => {
+  if (options.error && options.error.code === 'ECONNREFUSED') {
+    // End reconnecting on a specific error and flush all commands with
+    // a individual error
+    return new Error('The server refused the connection')
+  }
+  if (options.error && options.error.code === 'ETIMEDOUT') {
+    return new Error('Timeout occured while connecting to redis.')      
+  }
+  if (options.total_retry_time > 200 * 3) {
+    // End reconnecting after a specific timeout and flush all commands
+    // with a individual error
+    return new Error('Retry time exhausted')
+  }
+  if (options.attempt > 0 || options.times_connected > 0) {
+    // End reconnecting with built in error
+    return undefined
+  }
+  // reconnect after
+  return 100
 }
 
-const client = redis.createClient(REDIS_READ_PORT, REDIS_READ_HOST, {
+const readClient = redis.createClient(REDIS_READ_PORT, REDIS_READ_HOST, {
   password: REDIS_AUTH,
-  retry_strategy: function (options) {
-    if (options.error && options.error.code === 'ECONNREFUSED') {
-      return new Error('The server refused the connection')
-    }
-    if (options.error && options.error.code === 'ETIMEDOUT') {
-      return new Error('Timeout occured while connecting to redis.')      
-    }
-    if (options.total_retry_time > 200 * 3) {
-      return new Error('Retry time exhausted')
-    }
-    if (options.attempt > 0 || options.times_connected > 0) {
-      // this means "dont do retry"
-      return undefined
-    }
-    // reconnect after
-    // wouldnt go this return way never
-    return 100
-  }
+  retry_strategy: retryStrategy
 })
 
-const redisPoolRead = RedisConnectionPool('myRedisPoolRead', {
-  host: REDIS_READ_HOST,
-  port: REDIS_READ_PORT,
-  max_clients: REDIS_MAX_CLIENT ? REDIS_MAX_CLIENT : 50,
-  perform_checks: false,
-  database: 0,
-  options: REDIS_OPTIONS
+const writeClient = redis.createClient(REDIS_WRITE_PORT, REDIS_WRITE_HOST, {
+  password: REDIS_AUTH,
+  retry_strategy: retryStrategy
 })
 
-const redisPoolWrite = isProd ? RedisConnectionPool('myRedisPoolWrite', {
-  host: REDIS_WRITE_HOST,
-  port: REDIS_WRITE_PORT,
-  max_clients: 5,
-  perform_checks: false,
-  database: 0,
-  options: REDIS_OPTIONS
-}) : redisPoolRead
+const recommendNewsClient = isProd ? redis.createClient(REDIS_RECOMMEND_NEWS_PORT, REDIS_RECOMMEND_NEWS_HOST, {
+  password: REDIS_AUTH,
+  retry_strategy: retryStrategy
+}) : readClient
 
-const redisPoolRecommendNews = isProd ? RedisConnectionPool('redisPoolRecommendNews', {
-  host: REDIS_RECOMMEND_NEWS_HOST,
-  port: REDIS_RECOMMEND_NEWS_PORT,
-  max_clients: REDIS_MAX_CLIENT ? REDIS_MAX_CLIENT : 50,
-  perform_checks: false,
-  database: 0,
-  options: REDIS_OPTIONS
-}) : redisPoolRead
+const getAsync = promisify(readClient.get).bind(readClient)
+const setAsync = promisify(writeClient.set).bind(writeClient)
+const expireAsync = promisify(writeClient.expire).bind(writeClient)
+const mgetAsync = promisify(recommendNewsClient.mget).bind(recommendNewsClient)
 
-class TimeoutHandler {
-  constructor (callback) {
-    this.isResponded = false
-    this.timeout = REDIS_CONNECTION_TIMEOUT || 200 // 0.2s
-
-    this.destroy = this.destroy.bind(this)
-    this.init = this.init.bind(this)
-    this.init(callback)
-  }
-  init (callback) {
-    this.timeoutHandler = setInterval(() => {
-      this.timeout -= 100
-      if (this.isResponded) {
-        this.destroy()
-        return
-      }
-      if (this.timeout <= 0) {
-        this.destroy()
-        callback && callback({ error: 'Timeout occured while accessing data from redis.', data: null })
-      }
-    }, 100)
-  }
-  destroy () {
-    clearInterval(this.timeoutHandler)
-  }
+const promiseTimeout = (promise) => {
+  const time = REDIS_CONNECTION_TIMEOUT || 200
+  let timeout = new Promise((resolve, reject) => {
+    let timer = setTimeout(() => {
+      clearTimeout(timer)
+      reject('Timed out in '+ time + 'ms.')
+    }, time)
+  })
+  return Promise.race([
+    promise,
+    timeout
+  ])
 }
 
 const redisFetching = (url, callback) => {
   let start = Date.now()
-  let timeoutHandler = new TimeoutHandler(callback)
   let decodedUrl
+  let afterGet
+  let redisPoolReadError
   try {
     decodedUrl = decodeURIComponent(url)
   } catch (error) {
@@ -126,66 +84,30 @@ const redisFetching = (url, callback) => {
     decodedUrl = url
   }
   let beforeGet = Date.now() - start
-  client.get(decodedUrl, (error, data) => {
-    let afterGet = Date.now() - start
-    let redisPoolReadError
-    if (data === null) {
-      redisPoolReadError = 'Key does not exist.'
-    } else if (error) {
+  
+  promiseTimeout(getAsync(decodedUrl))
+    .then(data => {
+      afterGet = Date.now() - start
+      if (data === null) {
+        redisPoolReadError = 'Key does not exist.'
+      }
+      return data
+    })
+    .catch(error => {
+      afterGet = Date.now() - start
       redisPoolReadError = error
-    }
-    timeoutHandler.isResponded = true
-    timeoutHandler.destroy()
-
-    if (timeoutHandler.timeout <= 0) { return }
-
-    callback && callback({ error: redisPoolReadError, data })
-    
-    let timePeriod = Date.now() - start
-    if (timePeriod > 300) {
-      console.warn('[WARN]Redis operating total:', `${timePeriod}ms`, 'before get: ', `${beforeGet}ms`, 'after get: ', `${afterGet}ms`, decodedUrl)
-    }
-    timeoutHandler = null
-  })
-
-  // redisPoolRead.get(decodedUrl, (error, data) => {
-  //   let redisPoolReadError
-  //   if (data === null) {
-  //     redisPoolReadError = 'Key does not exist.'
-  //   } else if (error) {
-  //     redisPoolReadError = error
-  //   }
-  //   let afterGet = Date.now() - start
-
-  //   timeoutHandler.isResponded = true
-  //   timeoutHandler.destroy()
-    
-	// /*
-  //   redisPoolRead.ttl(decodedUrl, (err, dt) => {
-  //     if (!err && (dt === -1)) { // if the key exists but has no associated expire.
-  //       redisPoolWrite.del(decodedUrl, (e, d) => {
-  //         if (e) {
-  //           console.warn('[WARN] deleting key ', decodedUrl, 'from redis in fail ', e)
-  //         }
-  //       })
-  //     } else if (err) {
-  //       console.warn(`[WARN] fetching ttl in fail. ${decodedUrl} ${err}`)
-  //     }
-  //   })
-	// */
-    
-  //   if (timeoutHandler.timeout <= 0) { return }
-
-  //   callback && callback({ error: redisPoolReadError, data })
-  //   let timePeriod = Date.now() - start
-  //   if (timePeriod > 300) {
-  //     console.log('[WARN]Redis operating total:', `${timePeriod}ms`, 'before get: ', `${beforeGet}ms`, 'after get: ', `${afterGet}ms`, decodedUrl)
-  //   }
-  //   timeoutHandler = null
-  // })
+    })
+    .then(data => {
+      let timePeriod = Date.now() - start
+      if (afterGet > REDIS_CONNECTION_TIMEOUT) {
+        console.warn('[WARN]Redis operating total:', `${timePeriod}ms`, 'before get: ', `${beforeGet}ms`, 'after get: ', `${afterGet}ms`, decodedUrl)
+      }
+      callback && callback({ error: redisPoolReadError, data })
+    })
 }
+
 const redisWriting = (url, data, callback, timeout) => {
-  let timeoutHandler = new TimeoutHandler(callback)
+  const redisTimeout = timeout || REDIS_TIMEOUT || 5000
   let decodedUrl
   try {
     decodedUrl = decodeURIComponent(url)
@@ -194,40 +116,27 @@ const redisWriting = (url, data, callback, timeout) => {
     decodedUrl = url
   }
   debug('Going to Writing things to redis...')
-  redisPoolWrite.set(decodedUrl, data, (err) => {
-    timeoutHandler.isResponded = true
-    timeoutHandler.destroy()
-    if(err) {
-      console.error(`[ERROR] Write data to Redis in fail. ${decodedUrl} Error: ${err}`)
-    } else {
-      debug('Set timeout as:', timeout || REDIS_TIMEOUT)
-      redisPoolWrite.expire(decodedUrl, timeout || REDIS_TIMEOUT || 5000, function(error, d) {
-        if(error) {
-          console.error(`[ERROR] Set redis expire time in fail. ${decodedUrl} Error: ${err}`)
-        } else {
-          callback && callback()
-        }
-      })
-    }
-    timeoutHandler = null
-  })
+
+  promiseTimeout(setAsync(decodedUrl, data))
+    .then(() => expireAsync(decodedUrl, redisTimeout))
+    .catch(err => console.error(`[ERROR] Write data to Redis in fail. ${decodedUrl} Error: ${err}`))
+    .then(() => callback && callback())
+    .catch(err => console.error(`[ERROR] Set redis expire time in fail. ${decodedUrl} Error: ${err}`))
 }
+
 const redisFetchingRecommendNews = (field, callback) => {
-  let timeoutHandler = new TimeoutHandler(callback)
-  redisPoolRecommendNews.send_command('MGET', [ ...field ], function (err, data) {
-    timeoutHandler.isResponded = true
-    timeoutHandler.destroy()
-    if (timeoutHandler.timeout <= 0) { return }
-	console.info('>>> Fetch recommend news ' + field + ' failed')
-    callback && callback({ err, data })
-    timeoutHandler = null
-  })
+  promiseTimeout(mgetAsync([ ...field ]))
+    .then(data => callback && callback({ data }))
+    .catch(err =>{
+      console.error('>>> Fetch recommend news ' + field + ' failed', 'Error: ', err)
+      callback && callback({ err })
+    })
 }
 
 const insertIntoRedis = (req, res, next) => {
-  //redisWriting(req.url, res.dataString, () => {
-    // next()
-  //}, res.redisTTL)
+  redisWriting(req.url, res.dataString, () => {
+    next()
+  }, res.redisTTL)
 }
 
 const fetchFromRedis = (req, res, next) => {
@@ -245,13 +154,8 @@ const fetchFromRedis = (req, res, next) => {
 
 const fetchFromRedisForAPI = (req, res, next) => {
   debug('Trying to fetching data from redis...', req.url)
-  let start = Date.now()
   redisFetching(req.url, ({ error, data }) => {
     if (!error && data) {
-      let timePeriod = Date.now() - start
-      if (timePeriod > 500) {
-        console.log('[WARN]Fetch data from Redis.', `${timePeriod}ms`, req.url)
-      }
       res.header('Cache-Control', 'public, max-age=300')
       res.json(JSON.parse(data))
     } else {
