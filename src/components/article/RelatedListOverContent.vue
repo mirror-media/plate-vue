b3da6d6d5632c3e704bc9363d50e944e18012ae6
yeefun <szyeefun@gmail.com>
2019-10-04 12:47:10 +0800

    modify ga

--- src/components/article/RelatedListOverContent.vue
+++ src/components/article/RelatedListOverContent.vue
@@ -2,7 +2,7 @@
   <transition name="fade">
     <div class="relateds-over-content" v-show="showContent" :class="{ 'to-top': isToTop }">
       <div>
-        <a :href="`/story/${_get(articles[0], 'slug', '')}`" class="related" @click="sendGaClickEvent(`${relatedCategory ? 'related' : 'latest'} news`, 'left')">
+        <a :href="`/story/${_get(articles[0], 'slug', '')}`" class="related" @click="sendGaClickEvent('article', `${relatedCategory ? 'related' : 'latest'} news left`)">
           <div class="related__arrow"></div>
           <div class="related__txt">
             <div class="related__category">{{ relatedCategory || _get(articles[0], 'categories.0.title', '新聞') }}</div>
@@ -11,7 +11,7 @@
             </div>
           </div>
         </a>
-        <a :href="`/story/${_get(articles[1], 'slug', '')}`" class="related" @click="sendGaClickEvent(`${relatedCategory ? 'related' : 'latest'} news`, 'right')">
+        <a :href="`/story/${_get(articles[1], 'slug', '')}`" class="related" @click="sendGaClickEvent('article', `${relatedCategory ? 'related' : 'latest'} news right`)">
           <div class="related__txt">
             <div class="related__category">{{ relatedCategory || _get(articles[1], 'categories.0.title', '新聞') }}</div>
             <div class="related__title">
