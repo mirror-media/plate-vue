3345aee8c2222d2cf803fc76042bdbd56130ae79
Kai-Wen, Hsiung <kaiwenhsiung@gmail.com>
2020-04-29 18:26:10 +0800

    feat: add styles in container component

--- src/components/listWatch/ContainerListing.vue
+++ src/components/listWatch/ContainerListing.vue
@@ -36,11 +36,6 @@ export default {
   components: {
     UIWatch
   },
-  data () {
-    return {
-      title: '鐘錶列表'
-    }
-  },
   computed: {
     title () {
       return _.get(this.$store.state, ['commonData', 'categories', 'watchsingle', 'title'], '')
@@ -68,12 +63,71 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
+.title
+  font-size 26px
+  font-weight 900
+  color rgba(0, 0, 0, 0.87)
+  padding 0 0 0 13px
+  border-left 16px solid black
+  height 68px
+  display flex
+  align-items center
+
 .list
   list-style none
+  padding 0
+  width 100%
+  display flex
+  flex-wrap wrap
+  justify-content space-around
+  margin -2px 0 20px -12px
+  &__list-item
+    margin 22px 0 0 12px
 
 .list-item
   &__link
     display block
     width max-content
     height max-content
+
+@media (min-width: 768px)
+  .section
+    max-width 768px
+    margin 0 auto
+
+  .list
+    margin 20px 0 72px -12px
+    justify-content flex-start
+
+  .list-item
+    padding 0 24px
+
+@media (min-width: 1200px)
+  .section
+    max-width 1024px
+    margin 0 auto
+
+  .title
+    font-size 36px
+    padding 0 0 0 22px
+    border-left 24px solid black
+    height 76px
+
+  .list
+    margin 20px 0 72px 0
+    &__list-item
+      display flex
+      align-items center
+      margin 52px 0 0 0
+      &:not(:first-child):not(:nth-child(5n))
+        &:before
+          content ''
+          display block
+          width 2px
+          height 274px
+          background-color rgba(0, 0, 0, 0.1)
+          margin 0 30px 0 0
+
+  .list-item
+    padding 0 30px 0 0
 </style>
