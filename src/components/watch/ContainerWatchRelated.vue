83ae45e19dfeb267409e7c6884a2c0b4077a8733
Noah Tan <tempo0829@gmail.com>
2020-05-06 11:39:32 +0800

    feat: implement watch page

---
+++ src/components/watch/ContainerWatchRelated.vue
@@ -0,0 +1,30 @@
+<template>
+  <UIWatchRelated
+    :slug="post.slug"
+    :published-date="publishedDate"
+    :title="post.title"
+  />
+</template>
+<script>
+
+import UIWatchRelated from './UIWatchRelated.vue'
+import moment from 'moment'
+
+export default {
+  name: 'ContainerWatchRelated',
+  components: {
+    UIWatchRelated
+  },
+  props: {
+    post: {
+      type: Object,
+      required: true
+    }
+  },
+  computed: {
+    publishedDate () {
+      return moment(new Date(this.post.publishedDate)).format('YYYY/MM/DD')
+    }
+  }
+}
+</script>
