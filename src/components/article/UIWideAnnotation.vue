27dd915262bddfe71717307bd1b59bfbfdec2aab
HY Tan <tempo0829@gmail.com>
2020-06-24 13:18:20 +0800

    fix: annotation workaround (#1468)

--- src/components/article/UIWideAnnotation.vue
+++ src/components/article/UIWideAnnotation.vue
@@ -1,60 +1,57 @@
 <template>
   <div class="wide-annotation">
-    <span v-html="splitContent[0]" />
-    <template v-if="annotation">
-      <span
-        :class="{ active: openAnnotation }"
-        class="wide-annotation__text"
-        @click="openAnnotation = !openAnnotation"
-        v-text="annotation.text"
+    <template v-for="(data, index) in splitContent">
+      <UIWideAnnotationHandler
+        v-if="data.annotation"
+        :key="`${item.id}-${index}`"
+        :annotation="data"
       />
-      <div
-        v-show="openAnnotation"
-        class="annotation__content"
-      >
-        <div
-          class="content"
-          v-html="annotation.annotation"
-        />
-      </div>
+      <span
+        v-else
+        :key="`${item.id}-${index}`"
+        v-html="data"
+      />
     </template>
-    <span v-html="splitContent[1]" />
   </div>
 </template>
 
 <script>
-import { get } from 'lodash'
+import { flatten, get } from 'lodash'
+import UIWideAnnotationHandler from './UIWideAnnotationHandler.vue'
 
 export default {
   name: 'UIWideAnnotation',
+  components: {
+    UIWideAnnotationHandler
+  },
   props: {
     item: {
       type: Object,
       required: true
     }
   },
-  data () {
-    return {
-      openAnnotation: false
-    }
-  },
   computed: {
-    annotation () {
-      try {
-        const data = this.content.match(/<!--__ANNOTATION__=(.*)-->/gm)
-        return JSON.parse(data[0].match(/{(.*)}/gm))
-      } catch (error) {
-        console.log(error)
-        return undefined
-      }
-    },
     content () {
       return get(this.item, 'content.0', '')
     },
     splitContent () {
-      const data = this.content.split(/<!--__ANNOTATION__=(.*)-->/gm)
-      data.splice(1, 1)
-      return data
+      const data = this.content.split(/<!--__ANNOTATION__=/gm)
+
+      const split = data.map(item => {
+        if (item.match(/<!--(.*)-->/)) {
+          const qqq = item.split(/<!--(.*)-->/)
+          return [qqq[0].replace(/-->/, ''), qqq[2]]
+        }
+        return item
+      })
+      const flattenData = flatten(split)
+      const processed = flattenData.map(item => {
+        if (item.match(/^{(.*)}$/)) {
+          return JSON.parse(item)
+        }
+        return item
+      })
+      return processed
     }
   }
 }
@@ -67,49 +64,6 @@ export default {
     color: rgba(0,0,0,.87);
     font-size: 15px;
     line-height: 2;
-  }
-  .wide-annotation__text {
-    display: inline-flex;
-    align-items: center;
-    color: #3e67aa;
-    cursor: pointer;
-    &::after {
-      content: ''
-      width: 0;
-      height: 0;
-      margin-left: 5px;
-      border-style: solid;
-      border-width: 5px 5px 0 5px;
-      border-color: #3e67aa transparent transparent transparent;
-      transition transform .3s ease-in
-    }
-    &.active {
-      &::after {
-        transform: rotate(-180deg);
-      }
-    }
-  }
-  .annotation__content {
-    margin: 13px -calc((100% - 266px) / 2) 20px;
-    padding: 23px calc((100% - 266px) / 2);
-    color: rgba(0, 0, 0, 0.87);
-    font-size: 13px;
-    line-height: 1.5;
-    letter-spacing: 1px;
-    text-align: justify;
-    background-color: rgba(216, 216, 216, 0.3);
-    border-radius: 2px;
-    @media (min-width: 768px) {
-      margin: 13px 0 20px;
-      padding: 23px 37px;
-    }
-    .content {
-      width: 266px;
-      margin: 0 auto;
-      @media (min-width: 768px) {
-        width: 100%;
-      }
-    }
   }
 }
 
